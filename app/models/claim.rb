8910adfd7b784e459e7170f2b0c0a27898d72bb5
Stevie Graham <sjtgraham@mac.com>
2014-06-18 14:21:07 +0100

    Claim validations
    The "password" is optional so confirmation and presence validations
    on password and password_confirmation respectively should only be run
    when the "password" has been changed.

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,6 +1,9 @@
 class Claim < ActiveRecord::Base
   has_secure_password validations: false
+
   validates :password, confirmation: true
+  validates :password_confirmation,
+    presence: { if: -> { attribute_changed? :password_digest } }
 
   def reference
     KeyObfuscator.new.obfuscate(id)
--- spec/models/claim_spec.rb
+++ spec/models/claim_spec.rb
@@ -6,6 +6,15 @@ RSpec.describe Claim, :type => :model do
   describe 'validations' do
     it { is_expected.to_not validate_presence_of(:password) }
     it { is_expected.to     validate_confirmation_of(:password) }
+
+    describe 'password_confirmation when password has changed' do
+      before { subject.password = 'lol' }
+      it { is_expected.to validate_presence_of(:password_confirmation) }
+    end
+
+    describe 'password_confirmation when password has not changed' do
+      it { is_expected.to_not validate_presence_of(:password_confirmation) }
+    end
   end
 
   describe '#reference' do
