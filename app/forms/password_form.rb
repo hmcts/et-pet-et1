abd7ec175c9b7792ee804cc6ddae3b7336515b58
Stevie Graham <sjtgraham@mac.com>
2014-07-08 14:58:21 +0100

    Remove validations for user testing. DO NOT merge this into master ever! Thank you please. 😘

--- app/forms/password_form.rb
+++ app/forms/password_form.rb
@@ -1,6 +1,3 @@
 class PasswordForm < Form
   attributes :password, :password_confirmation
-
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: { if: -> { password.present? } }
 end
