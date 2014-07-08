d5a5b7ee51d497fd0c39a8f857cd2f81fc92d5b7
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2014-07-08 17:13:55 +0100

    added new field to your details page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703094644) do
+ActiveRecord::Schema.define(version: 20140708131259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140703094644) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "telephone_number"
+    t.string   "country"
   end
 
   create_table "claimants", force: true do |t|
