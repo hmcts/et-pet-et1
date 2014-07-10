b85fad4e3d22f6453419606043332840df60c05b
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2014-07-10 13:46:53 +0100

    added country field to claimaint

--- app/models/claimant.rb
+++ app/models/claimant.rb
@@ -2,8 +2,8 @@ class Claimant < ActiveRecord::Base
   belongs_to :claim
   has_one :address, as: :addressable
 
-  delegate :building, :street, :locality, :county, :post_code, :telephone_number,
-    :building=, :street=, :locality=, :county=, :post_code=, :telephone_number=,
+  delegate :building, :street, :locality, :county, :post_code, :telephone_number, :country,
+    :building=, :street=, :locality=, :county=, :post_code=, :telephone_number=, :country=,
     to: :address, prefix: true
 
   def address
