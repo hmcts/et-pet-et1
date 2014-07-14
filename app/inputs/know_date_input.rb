ba591f3318470075a035204f98a34b7543a10cb3
Stevie Graham <sjtgraham@mac.com>
2014-07-14 12:30:06 +0100

    The custom know date input is a B on CodeClimate. Breaking apart the complex method to improve the code quality and make it more readable

--- app/inputs/know_date_input.rb
+++ app/inputs/know_date_input.rb
@@ -1,25 +1,37 @@
 class KnowDateInput < SimpleForm::Inputs::Base
-
   def input(wrapper_options = nil)
-    value    = object.send attribute_name
-    defaults = SimpleForm.wrapper(:default).defaults
-
-    template.instance_exec(self) do |b|
-      content_tag(defaults[:tag], class: defaults[:class] + ['know-date-input']) do
-        3.downto(1) do |i|
-          name = "#{b.object_name}[#{b.attribute_name}(#{i}i)]"
-          id   = name.gsub(/([\[\(])|(\]\[)/, '_').gsub(/[\]\)]/, '')
-
-          key, _ = ActionView::Helpers::DateTimeSelector::POSITION.rassoc i
-          maxlength = key == :year ? 4 : 2
-
-          concat(content_tag(defaults[:tag], class: defaults[:class] + ["know-date-#{key}"]) do
-            concat label_tag id, t("datetime.prompts.#{key}")
-            concat text_field_tag name, value.try(key),
-              id: id, class: b.input_html_classes, maxlength: maxlength
-          end)
-        end
-      end
+    template.content_tag(defaults[:tag], class: defaults[:class] + ['know-date-input']) do
+      3.downto(1).inject('') { |memo, i| memo + input_segment_for(index: i) }.html_safe
     end
+  end
+
+  def input_segment_for(index:)
+    id   = name_for(index: index).gsub(/([\[\(])|(\]\[)/, '_').gsub(/[\]\)]/, '')
+
+    key, _ = ActionView::Helpers::DateTimeSelector::POSITION.rassoc index
+    maxlength = key == :year ? 4 : 2
+
+    template.content_tag(defaults[:tag], class: defaults[:class] + ["know-date-#{key}"]) do
+      template.label_tag(id, I18n.t("datetime.prompts.#{key}")) +
+      template.text_field_tag(name_for(index: index), value_for(key: key), id: id, class: input_html_classes, maxlength: maxlength)
+    end
+  end
+
+  private
+
+  def name_for(index:)
+    "#{object_name}[#{attribute_name}(#{index}i)]"
+  end
+
+  def id_for(index:)
+
+  end
+
+  def defaults
+    SimpleForm.wrapper(:default).defaults
+  end
+
+  def value_for(key:)
+    object.send(attribute_name).try(key)
   end
 end
