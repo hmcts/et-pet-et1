d694d28f8913509aaf6b1cf53276b4b01c1b0376
Stevie Graham <sjtgraham@mac.com>
2014-07-14 13:51:54 +0100

    DRY up Respondent address delegation

--- app/models/respondent.rb
+++ app/models/respondent.rb
@@ -2,13 +2,11 @@ class Respondent < ActiveRecord::Base
   belongs_to :claim
   has_many :addresses, as: :addressable
 
-  delegate :building, :street, :locality, :county, :post_code, :telephone_number,
-    :building=, :street=, :locality=, :county=, :post_code=, :telephone_number=,
-    to: :address, prefix: true
+  ADDRESS_ATTRIBUTES = %i<building street locality county post_code
+                          telephone_number>.flat_map { |a| [a, :"#{a}="] }
 
-  delegate :building, :street, :locality, :county, :post_code, :telephone_number,
-    :building=, :street=, :locality=, :county=, :post_code=, :telephone_number=,
-    to: :work_address, prefix: true
+  delegate *ADDRESS_ATTRIBUTES, to: :address, prefix: true
+  delegate *ADDRESS_ATTRIBUTES, to: :work_address, prefix: true
 
   def address
     addresses.first
