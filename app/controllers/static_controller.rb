f3b1094b17ca64820e653e92bb1eee803a3bfb1b
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2014-07-15 12:29:34 +0100

    added review page

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,4 @@
+class StaticController < ApplicationController
+  def review
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static/review'
+
   resource :claim, only: %i<create update>, path: 'apply' do
     member do
       get ':page', to: 'claims#show', as: :page
