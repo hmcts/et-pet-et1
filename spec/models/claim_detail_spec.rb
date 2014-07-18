728d3973cf97a753c77866990dc2f40922727fde
Stevie Graham <sjtgraham@mac.com>
2014-07-18 16:25:23 +0100

    Fee calculations:
     - Calculates fees for claim type 2, i.e. non-discrimination and
       non-unfair-dissmissal claims
     - Adds Claim#alleges_discrimination_or_unfair_dismissal? which
       delgates through to ClaimDetail#alleges_discrimination_or_unfair_dismissal?
     - Adds ClaimDetail#alleges_discrimination_or_unfair_dismissal?

--- spec/models/claim_detail_spec.rb
+++ spec/models/claim_detail_spec.rb
@@ -3,11 +3,36 @@ require 'rails_helper'
 RSpec.describe ClaimDetail, :type => :model do
   it { is_expected.to belong_to :claim }
 
-  describe 'bitmasked attributes' do
-    let(:subject) { ClaimDetail.new }
+  let(:subject) { ClaimDetail.new }
 
+  describe 'bitmasked attributes' do
     %i<discrimination_claims pay_claims desired_outcomes>.each do |attr|
       specify { expect(subject.send attr).to be_an(Array) }
+    end
+  end
+
+  describe '#alleges_discrimination_or_unfair_dismissal?' do
+    context 'when there are no claims of discrimination or unfair dismissal' do
+      its(:alleges_discrimination_or_unfair_dismissal?) { is_expected.to be false }
+    end
+
+    context 'when there is a claim of discrimination' do
+      before { subject.discrimination_claims << :race }
+      its(:alleges_discrimination_or_unfair_dismissal?) { is_expected.to be true }
+    end
+
+    context 'when there is a claim of unfair dismissal' do
+      before { subject.is_unfair_dismissal = true }
+      its(:alleges_discrimination_or_unfair_dismissal?) { is_expected.to be true }
+    end
+
+    context 'when there are claims of both discrimination and unfair dismissal' do
+      before do
+        subject.discrimination_claims << :race
+        subject.is_unfair_dismissal = true
+      end
+
+      its(:alleges_discrimination_or_unfair_dismissal?) { is_expected.to be true }
     end
   end
 end
--- spec/services/claim_fee_calculator_spec.rb
+++ spec/services/claim_fee_calculator_spec.rb
@@ -4,69 +4,165 @@ RSpec.describe ClaimFeeCalculator, type: :service do
 
   describe '.calculate' do
     describe 'calculating the application fee' do
-      context 'with a single claimant' do
-        before { allow(claim).to receive(:claimant_count).and_return 1 }
+      context 'when the claim pertains to discrimination or unfair dismissal' do
+        before do
+          allow(claim).
+            to receive(:alleges_discrimination_or_unfair_dismissal?).
+            and_return true
+        end
 
-        it 'is £250' do
-          expect(calculation.application_fee).to eq '£250'
+        context 'with a single claimant' do
+          before { allow(claim).to receive(:claimant_count).and_return 1 }
+
+          it 'is £250' do
+            expect(calculation.application_fee).to eq '£250'
+          end
+        end
+
+        context "with 2..10 claimants" do
+          before { allow(claim).to receive(:claimant_count).and_return *(2..10) }
+
+          it "is £500" do
+            (2..10).each { expect(calculation.application_fee).to eq '£500' }
+          end
+        end
+
+        context "with 11..200 claimants" do
+          before { allow(claim).to receive(:claimant_count).and_return *(11..200) }
+
+          it "is £1000" do
+            (11..200).each { expect(calculation.application_fee).to eq '£1000' }
+          end
+        end
+
+        context 'with 201 or more claimants' do
+          before { allow(claim).to receive(:claimant_count).and_return Float::INFINITY }
+
+          it "is £1500" do
+            expect(calculation.application_fee).to eq '£1500'
+          end
         end
       end
 
-      context "with 2..10 claimants" do
-        before { allow(claim).to receive(:claimant_count).and_return *(2..10) }
-
-        it "is £500" do
-          (2..10).each { expect(calculation.application_fee).to eq '£500' }
+      context 'when the claim does not pertain to discrimination or unfair dismissal' do
+        before do
+          allow(claim).
+            to receive(:alleges_discrimination_or_unfair_dismissal?).
+            and_return false
         end
-      end
 
-      context "with 11..200 claimants" do
-        before { allow(claim).to receive(:claimant_count).and_return *(11..200) }
+        context 'with a single claimant' do
+          before { allow(claim).to receive(:claimant_count).and_return 1 }
 
-        it "is £1000" do
-          (11..200).each { expect(calculation.application_fee).to eq '£1000' }
+          it 'is £160' do
+            expect(calculation.application_fee).to eq '£160'
+          end
         end
-      end
 
-      context 'with 201 or more claimants' do
-        before { allow(claim).to receive(:claimant_count).and_return Float::INFINITY }
+        context "with 2..10 claimants" do
+          before { allow(claim).to receive(:claimant_count).and_return *(2..10) }
 
-        it "is £1500" do
-          expect(calculation.application_fee).to eq '£1500'
+          it "is £320" do
+            (2..10).each { expect(calculation.application_fee).to eq '£320' }
+          end
+        end
+
+        context "with 11..200 claimants" do
+          before { allow(claim).to receive(:claimant_count).and_return *(11..200) }
+
+          it "is £640" do
+            (11..200).each { expect(calculation.application_fee).to eq '£640' }
+          end
+        end
+
+        context 'with 201 or more claimants' do
+          before { allow(claim).to receive(:claimant_count).and_return Float::INFINITY }
+
+          it "is £960" do
+            expect(calculation.application_fee).to eq '£960'
+          end
         end
       end
     end
 
     describe 'calculating the hearing fee' do
-      context 'with a single claimant' do
-        before { allow(claim).to receive(:claimant_count).and_return 1 }
+      context 'when the claim pertains to discrimination or unfair dismissal' do
+        before do
+          allow(claim).
+            to receive(:alleges_discrimination_or_unfair_dismissal?).
+            and_return true
+        end
 
-        it 'is £950' do
-          expect(calculation.hearing_fee).to eq '£950'
+        context 'with a single claimant' do
+          before { allow(claim).to receive(:claimant_count).and_return 1 }
+
+          it 'is £950' do
+            expect(calculation.hearing_fee).to eq '£950'
+          end
+        end
+
+        context "with 2..10 claimants" do
+          before { allow(claim).to receive(:claimant_count).and_return *(2..10) }
+
+          it "is £1900" do
+            (2..10).each { expect(calculation.hearing_fee).to eq '£1900' }
+          end
+        end
+
+        context "with 11..200 claimants" do
+          before { allow(claim).to receive(:claimant_count).and_return *(11..200) }
+
+          it "is £3800" do
+            (11..200).each { expect(calculation.hearing_fee).to eq '£3800' }
+          end
+        end
+
+        context 'with 201 or more claimants' do
+          before { allow(claim).to receive(:claimant_count).and_return Float::INFINITY }
+
+          it "is £5700" do
+            expect(calculation.hearing_fee).to eq '£5700'
+          end
         end
       end
 
-      context "with 2..10 claimants" do
-        before { allow(claim).to receive(:claimant_count).and_return *(2..10) }
-
-        it "is £1900" do
-          (2..10).each { expect(calculation.hearing_fee).to eq '£1900' }
+      context 'when the claim does not pertain to discrimination or unfair dismissal' do
+        before do
+          allow(claim).
+            to receive(:alleges_discrimination_or_unfair_dismissal?).
+            and_return false
         end
-      end
 
-      context "with 11..200 claimants" do
-        before { allow(claim).to receive(:claimant_count).and_return *(11..200) }
+        context 'with a single claimant' do
+          before { allow(claim).to receive(:claimant_count).and_return 1 }
 
-        it "is £3800" do
-          (11..200).each { expect(calculation.hearing_fee).to eq '£3800' }
+          it 'is £230' do
+            expect(calculation.hearing_fee).to eq '£230'
+          end
         end
-      end
 
-      context 'with 201 or more claimants' do
-        before { allow(claim).to receive(:claimant_count).and_return Float::INFINITY }
+        context "with 2..10 claimants" do
+          before { allow(claim).to receive(:claimant_count).and_return *(2..10) }
 
-        it "is £5700" do
-          expect(calculation.hearing_fee).to eq '£5700'
+          it "is £460" do
+            (2..10).each { expect(calculation.hearing_fee).to eq '£460' }
+          end
+        end
+
+        context "with 11..200 claimants" do
+          before { allow(claim).to receive(:claimant_count).and_return *(11..200) }
+
+          it "is £920" do
+            (11..200).each { expect(calculation.hearing_fee).to eq '£920' }
+          end
+        end
+
+        context 'with 201 or more claimants' do
+          before { allow(claim).to receive(:claimant_count).and_return Float::INFINITY }
+
+          it "is £1380" do
+            expect(calculation.hearing_fee).to eq '£1380'
+          end
         end
       end
     end
