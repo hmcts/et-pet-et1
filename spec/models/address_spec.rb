e2cec1aebf1428c7b9df7c65b67eea7048f24582
Stevie Graham <sjtgraham@mac.com>
2014-07-21 13:42:33 +0100

    Address#empty?
    - Address#empty? is true unless all attributes are present.
    - RespondentForm#worked_at_different_address is true unless
      Repsondent#work_address is empty?

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Address, :type => :model do
+RSpec.describe Address, type: :model do
   it { is_expected.to belong_to :addressable }
+
+  describe '#empty?' do
+    context 'when any attributes are blank' do
+      let(:subject) { Address.new }
+
+      specify { expect(subject).to be_empty }
+    end
+
+    context 'when no attributes are blank' do
+      let(:subject) do
+        Address.new building: 'lol', street: 'lol', locality: 'lol', county: 'lol',
+          post_code: 'lol', telephone_number: 'lol', country: 'lol'
+      end
+
+      specify { expect(subject).not_to be_empty }
+    end
+  end
 end
