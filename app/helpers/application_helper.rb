4f06865ab6f03d7259e48efff01de0beadecd13d
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2014-07-25 15:11:23 +0100

    @jasiek helped to get markdown working on nested :yield

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    @markdown.render(content).html_safe
+  end
 end
