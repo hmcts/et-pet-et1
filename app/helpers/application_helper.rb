9e5f08888d2ee9f8fa985e58ab8ecdde7784448e
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2014-07-28 14:29:20 +0100

    removed markdown help that @jasiek added for me

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  def markdown(content)
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
-    @markdown.render(content).html_safe
-  end
 end
