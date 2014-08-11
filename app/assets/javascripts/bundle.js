23c4da53a0036fb52ac0f2c33800d018ac780017
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2014-08-11 12:36:55 +0100

    updated checkboxes JS to spec Sjors outlined

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -17,19 +17,50 @@ module.exports = (function() {
   });
 })();
 },{}],3:[function(require,module,exports){
-// Toggles disabled groups of adjacent checkboxes
-
+/* Toggles disabled groups of adjacent checkboxes
+* assumes structure: .related-checkboxes-root + .related-checkboxes-collection
+*/
 module.exports = (function() {
-  var root = $('.related-checkboxes-root'),
-    collections = $('.related-checkboxes-collection');
+  var rootCheckbox = $('.related-checkboxes-root'),
+    toggleRootCheckbox = function(array, root) {
+      var checkbox = root.find('input');
+      return checkbox.prop({
+        checked : array.length
+      });
+    },
+    toggleCheckboxes = function(checked, array, val) {
+      if(checked){
+        return array.push(val);
+      } else {
+        return array.pop(array.indexOf(val));
+      }
+    };
 
-  collections.each(function(i, collection){
-    var selected = [],
-      checkboxes = $(collection).find('input');
-    //
-    checkboxes.on('change', function(){
+  rootCheckbox.each(function(i, root) {
+    var main = $(root),
+      collection = main.next('.related-checkboxes-collection'),
+      selectedArray = [],
+      checkboxes = collection.find('input');
 
+    main.on('change', function(){
+      var checked = main.is(':checked');
+      if(!checked){
+        $.each(selectedArray, function(i,val){
+          $(checkboxes[val]).prop('checked' , false);
+        });
+        selectedArray = [];
+      }
     });
+
+    checkboxes.each(function(index, el) {
+      var checked,
+        checkbox = $(el);
+      checkbox.on('change', function() {
+        checked = checkbox.is(':checked');
+        toggleCheckboxes(checked, selectedArray, index);
+        toggleRootCheckbox(selectedArray, main);
+      });
+    })
   });
 
 })();
