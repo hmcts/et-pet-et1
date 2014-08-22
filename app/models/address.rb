e8a5b541dd23c64ba6f616f4c1a61e563c25588e
Stevie Graham <sjtgraham@mac.com>
2014-08-22 17:12:18 +0100

    Revert "Address#empty?"
    This reverts commit 555c5be7acb38b463857f52c6fc0a3bd2a827d4f.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,3 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
-
-  def empty?
-    %i<building street locality county post_code country telephone_number>.all? do |attr|
-      send(attr).blank?
-    end
-  end
-
 end
