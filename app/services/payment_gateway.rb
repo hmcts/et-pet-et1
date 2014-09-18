f4307fb0cd574adf9a5239d541259509d3b68001
Stevie Graham <sjtgraham@mac.com>
2014-09-18 22:03:31 +0100

    PaymentGateway
    Assume payment gateway is up until we find it isn't. Because classes are
    reloaded every request in development, it creates a race condition where
    PaymentGateway.available? may be nil in the request. Because classes are
    cached in test and also eager loaded in production this shouldn't be a problem
    in those environments, but it's annoying in development

--- app/services/payment_gateway.rb
+++ app/services/payment_gateway.rb
@@ -1,5 +1,10 @@
 module PaymentGateway
-  @available = nil
+  # Assume payment gateway is up until we find it isn't. Because classes are
+  # reloaded every request in development, it creates a race condition where
+  # PaymentGateway.available? may be nil in the request. Because classes are
+  # cached in test and also eager loaded in production this shouldn't be a problem
+  # in those environments, but it's annoying in development
+  @available = true
   MUTEX = Mutex.new
 
   TASK = PeriodicTask.new(every: 5.seconds, run_immediately: !Rails.env.test?) do
