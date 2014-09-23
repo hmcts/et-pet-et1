bb2a7ef46b5e5c6a13d81ca03ec5360bf5c71319
Phil Thompson <phil.thompson@digital.justice.gov.uk>
2014-09-23 16:11:33 +0100

    completion page - reuse confirmation email

--- spec/support/messages.rb
+++ spec/support/messages.rb
@@ -12,4 +12,16 @@ module Messages
   def completion_message(reference)
     format I18n.t('claim_reviews.confirmation.complete', reference: reference)
   end
+
+  def payment_message
+    format I18n.t('claim_reviews.confirmation.fee_paid')
+  end
+
+  def remission_help
+    format I18n.t('claim_reviews.confirmation.remission_help')
+  end
+
+  def table_heading(heading)
+    I18n.t("claim_reviews.confirmation.details.#{heading}")
+  end
 end
