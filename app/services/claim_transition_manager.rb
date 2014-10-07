9c963fa21f90a6a5d5f5d6a9b904982f0ca7098c
Dan Palmer <boss@bitrecursive.com>
2014-10-07 21:30:49 +0100

    Update the claim transition manager to reflect the break up of the form component into many forms

--- app/services/claim_transition_manager.rb
+++ app/services/claim_transition_manager.rb
@@ -3,6 +3,8 @@ class ClaimTransitionManager < TransitionManager
   transition :claimant       => :representative
   transition :representative => :respondent
   transition :respondent     => :employment
-  transition :employment     => :claim
-  transition :claim          => :review
+  transition :employment     => :claim_type
+  transition :claim_type     => :claim_details
+  transition :claim_details  => :claim_outcome
+  transition :claim_outcome  => :review
 end
