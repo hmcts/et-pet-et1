674c37f2f422581654bf75d47e3d6533c4d87fb2
Dan Palmer <boss@bitrecursive.com>
2014-10-07 15:49:30 +0100

    Update class name on claim outcome form

--- app/forms/claim_outcome_form.rb
+++ app/forms/claim_outcome_form.rb
@@ -1,8 +1,7 @@
-class ClaimForm < Form
+class ClaimOutcomeForm < Form
   attributes :desired_outcomes, :other_outcome
 
   validates :other_outcome, length: { maximum: 2500 }
-
 
   private def target
     resource
