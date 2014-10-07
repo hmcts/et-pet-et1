cfda4ab53c7c5ad094e5cb68ad60381b395ce013
dan-palmer <Dan@Dans-MacBook-Pro.local>
2014-10-07 14:07:17 +0100

    Adds unfair dismissal & other claim details attributes to the ClaimTypeForm.

--- app/forms/claim_type_form.rb
+++ app/forms/claim_type_form.rb
@@ -1,8 +1,8 @@
 class ClaimTypeForm < Form
   attr_accessor :is_other_type_of_claim
 
-  attributes :discrimination_claims, :pay_claims, :is_whistleblowing,
-    :send_claim_to_whistleblowing_entity
+  attributes :is_unfair_dismissal, :discrimination_claims, :pay_claims,
+    :is_whistleblowing, :send_claim_to_whistleblowing_entity, :other_claim_details
 
   private def target
     resource
