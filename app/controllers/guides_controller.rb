923f4b144de7f12d17b07a9dd89133ed455b2a22
Dan Palmer <boss@bitrecursive.com>
2014-10-10 18:32:42 +0100

    Adds the guides page using the new markdown rendering for the individual guides

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,13 @@
 class GuidesController < ApplicationController
   def show
+    markup_directory = Rails.root.join( *%w{ app views guides markdown } )
+
+    files = %w{
+      fees
+      acas_early_conciliation
+      writing_your_claim_statement
+    }
+
+    @guide_presenter = GuidePresenter.new( markup_directory, files, MarkdownRenderer.new )
   end
 end
