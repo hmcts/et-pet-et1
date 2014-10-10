f059725d9774ecc302b1cadc75e4e3f6a63a90bd
David Plews <david.plews@digital.justice.gov.uk>
2014-10-10 14:00:45 +0100

    Changed fx- prefix to reveal-

--- app/assets/javascripts/modules/moj.reveal-pub-sub.js
+++ app/assets/javascripts/modules/moj.reveal-pub-sub.js
@@ -22,8 +22,8 @@ module.exports = (function() {
    * Bind a delegated click event on the containers
    * with publish elements.
    *
-   * Container class: .fx-publish-delegate
-   * Publisher class: .fx-publish-publisher
+   * Container class: .reveal-publish-delegate
+   * Publisher class: .reveal-publish-publisher
    *
    * Publisher attributes:
    * value:       The value will be used to determain if a subscriber should show/hide.
@@ -31,20 +31,20 @@ module.exports = (function() {
    * data-target: The event name to subscribe to.
    *
    * Example:
-   * <div class="fx-publish-delegate">
+   * <div class="reveal-publish-delegate">
         <input  name="sample" type="radio"
-                class="fx-publish-publisher"
+                class="reveal-publish-publisher"
                 value="true"
                 data-target="eventName" />
 
         <input  name="sample" type="radio"
-                class="fx-publish-publisher"
+                class="reveal-publish-publisher"
                 value="false"
                 data-target="eventName" />
       </div>
    */
   revealPubSub.bindPublish = function() {
-    $('.fx-publish-delegate').on('click', '.fx-publish-publisher', function(e) {
+    $('.reveal-publish-delegate').on('click', '.reveal-publish-publisher', function(e) {
       e.stopPropagation(); // stop nested elements to fire event twice
       var $el = $(e.target);
       $.publish($el.data('target'), $el.val());
@@ -54,7 +54,7 @@ module.exports = (function() {
   /**
    * Bind elements that subscribe to events.
    *
-   * Subscriber class: .fx-subscribe
+   * Subscriber class: .reveal-subscribe
    *
    * Subscriber attributes:
    * data-target:       The event name to subscibe to.
@@ -62,14 +62,14 @@ module.exports = (function() {
    *                    this corresponds to the puplisher value attribute
    *
    * Example:
-   *  <div  class="fx-subscribe"
+   *  <div  class="reveal-subscribe"
             data-target="eventName"
             data-show-array="['true',....]">
               // Further HTML here
       </div>
    */
   revealPubSub.bindSubscribe = function() {
-    $('.fx-subscribe').is(function(idx, el) {
+    $('.reveal-subscribe').is(function(idx, el) {
       var $el = $(el);
       $.subscribe($el.data('target'), function(event, val) {
         if ($.inArray(val, $el.data('show-array')) !== -1) {
