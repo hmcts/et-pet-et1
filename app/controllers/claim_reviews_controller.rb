db4e11b0cc76973d33bb46b21f463289d373b075
Dan Palmer <boss@bitrecursive.com>
2014-10-17 10:47:24 +0100

    Extract private method for attempting to send confirmation email

--- app/controllers/claim_reviews_controller.rb
+++ app/controllers/claim_reviews_controller.rb
@@ -3,15 +3,17 @@ class ClaimReviewsController < ApplicationController
 
   def update
     claim.submit!
-
-    if email_addresses.any?
-      BaseMailer.confirmation_email(claim, email_addresses).deliver_later
-    end
-
+    attempt_send_confirmation_email
     redirect_to claim.payment_required? ? page_claim_path(page: 'pay') : claim_confirmation_path
   end
 
   private
+
+  def attempt_send_confirmation_email
+    if email_addresses.any?
+      BaseMailer.confirmation_email(claim, email_addresses).deliver_later
+    end
+  end
 
   def email_addresses
     addresses = params[:confirmation_email]
