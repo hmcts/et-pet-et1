328c1680d34b420a55497a5806675090c63f2aad
Stevie Graham <sjtgraham@mac.com>
2014-10-21 14:15:16 +0100

    Remember selection "was employed" selection on Employment details
    - Value of was employed is computed whether or not a employment
      relation exists on the claim. Unfortunately this means it's
      pre-selected as "No" the first time the user gets the page.
      We can revisit this later.

--- spec/forms/employment_form_spec.rb
+++ spec/forms/employment_form_spec.rb
@@ -3,4 +3,22 @@ require 'rails_helper'
 RSpec.describe EmploymentForm, :type => :form do
   it_behaves_like 'it parses and validates multiparameter dates',
     :start_date, :end_date, :notice_period_end_date, :new_job_start_date
+
+  subject { described_class.new { |f| f.resource = Claim.new } }
+
+  describe '#was_employed' do
+    context 'when the underlying claim does not have an employment relation' do
+      it 'is false' do
+        expect(subject.was_employed).to be false
+      end
+    end
+
+    context 'when the underlying claim does not have an employment relation' do
+      before { subject.resource.employment = Employment.new }
+
+      it 'is true' do
+        expect(subject.was_employed).to be true
+      end
+    end
+  end
 end
