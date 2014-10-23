07ffb9aa8cbdb87341b558ca8c4b008f43411c25
Dan Palmer <boss@bitrecursive.com>
2014-10-23 14:08:36 +0100

    Split the custom validations into two distinct validations for user sessions

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,18 +2,24 @@ class UserSession
   include ActiveModel::Validations, ActiveModel::Model
 
   attr_accessor :reference, :password, :email_address
-  validates_presence_of :password, :reference
-  validate :authenticates
+
+  validates :password, :reference, presence: true
+  validate :presence_of_claim
+  validate :password_authenticates, if: :claim
 
   def claim
     @claim ||= Claim.find_by_reference(reference)
   end
 
-  private def authenticates
-    if claim
-      errors.add(:password, I18n.t('errors.user_session.invalid')) unless claim.authenticate password
-    else
-      errors.add(:reference, I18n.t('errors.user_session.not_found'))
+  private 
+
+  def presence_of_claim
+    errors.add(:reference, I18n.t('errors.user_session.not_found')) unless claim
+  end
+
+  def password_authenticates
+    unless claim.authenticate(password)
+      errors.add(:password, I18n.t('errors.user_session.invalid'))
     end
   end
 end
