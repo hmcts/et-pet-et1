72ff8e50ed14cb016a4b36a6cf2e456b0588942f
Paul Battley <pbattley@gmail.com>
2014-11-07 14:39:13 +0000

    Extract explanatory methods

--- app/services/claim_fee_calculator/remission_calculator.rb
+++ app/services/claim_fee_calculator/remission_calculator.rb
@@ -8,26 +8,34 @@ class ClaimFeeCalculator
       end
     end
 
-    private def calculate_for_multiple_claimants
-      factor = case claimant_count
-        when 2..10   then 2
-        when 11..200 then 4
-        when 201..Float::INFINITY then 6
-      end
+    private
 
-      if claimant_count - remission_claimant_count < factor
-        (full_application_fee / factor) * (claimant_count - remission_claimant_count)
+    def calculate_for_multiple_claimants
+      if non_remission_claimant_count < multiple_claimant_factor
+        (full_application_fee / multiple_claimant_factor) * non_remission_claimant_count
       else
         full_application_fee
       end
     end
 
-    private def calculate_for_individual_claimant
+    def calculate_for_individual_claimant
       if claim.remission_claimant_count > 0
         0
       else
         full_application_fee
       end
+    end
+
+    def multiple_claimant_factor
+      case claimant_count
+      when 2..10   then 2
+      when 11..200 then 4
+      when 201..Float::INFINITY then 6
+      end
+    end
+
+    def non_remission_claimant_count
+      claimant_count - remission_claimant_count
     end
 
     delegate :claimant_count, :remission_claimant_count, to: :claim
