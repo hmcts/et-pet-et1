402e04e9ace3f6d9cd56a9351080876fa2f0349b
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2014-11-11 16:55:06 +0000

    double checking browsrify.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
-var jqueryPubSub = require('./modules/moj.jquery-pub-sub');
-var polyfillDetail = require('./polyfills/polyfill.details');
-var revealPubSub = require('./modules/moj.reveal-pub-sub');
-var selectedOption = require('./modules/moj.selected-option');
-var formHintReveal = require('./modules/moj.reveal-hints');
-var removeMultiple = require('./modules/moj.remove-multiple');
+var jqueryPubSub = require('./modules/moj.jquery-pub-sub'),
+ polyfillDetail = require('./polyfills/polyfill.details'),
+ revealPubSub = require('./modules/moj.reveal-pub-sub'),
+ selectedOption = require('./modules/moj.selected-option'),
+ formHintReveal = require('./modules/moj.reveal-hints'),
+ removeMultiple = require('./modules/moj.remove-multiple');
 
 revealPubSub.init();
