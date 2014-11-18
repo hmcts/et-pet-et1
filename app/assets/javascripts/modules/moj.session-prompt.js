a7bc9454ad11e90b6187dbaa3156207ebb27e4af
David Plews <david.plews@digital.justice.gov.uk>
2014-11-18 15:41:06 +0000

    Linting the JS

--- app/assets/javascripts/modules/moj.session-prompt.js
+++ app/assets/javascripts/modules/moj.session-prompt.js
@@ -1,73 +1,73 @@
 // Manages session timeout & displays prompt prior to session expiry
-module.exports = (function() {
+module.exports = (function () {
 
-  var SECOND = 1000,
-      MINUTE = 60 * SECOND,
-      FIVE_MINUTES = 5 * MINUTE,
-      FIFTY_FIVE_MINUTES = 55 * MINUTE;
+    var SECOND = 1000,
+        MINUTE = 60 * SECOND,
+        FIVE_MINUTES = 5 * MINUTE,
+        FIFTY_FIVE_MINUTES = 55 * MINUTE;
 
-  return {
+    return {
 
-    timerRef: null,
+        timerRef: null,
 
-    init: function(){
-      this.counter = FIVE_MINUTES;
-      this.setPromptExtendSessionClickEvent();
-      this.startSessionTimer();
-    },
+        init: function () {
+            this.counter = FIVE_MINUTES;
+            this.setPromptExtendSessionClickEvent();
+            this.startSessionTimer();
+        },
 
-    startSessionTimer: function(){
-      setTimeout($.proxy(this.timeoutPrompt, this), FIFTY_FIVE_MINUTES);
-    },
+        startSessionTimer: function () {
+            setTimeout($.proxy(this.timeoutPrompt, this), FIFTY_FIVE_MINUTES);
+        },
 
-    setPromptExtendSessionClickEvent: function(){
-      $("#session_prompt_continue_btn").unbind().on("click", $.proxy(this.refreshSession, this));
-    },
+        setPromptExtendSessionClickEvent: function () {
+            $("#session_prompt_continue_btn").unbind().on("click", $.proxy(this.refreshSession, this));
+        },
 
-    timeoutPrompt: function(){
-      this.timerRef = setInterval($.proxy(this.promptUpdate, this), SECOND);
-      this.togglePromptVisibility();
-    },
+        timeoutPrompt: function () {
+            this.timerRef = setInterval($.proxy(this.promptUpdate, this), SECOND);
+            this.togglePromptVisibility();
+        },
 
-    promptUpdate: function(){
-      if(this.counter === 0){
-        this.expireSession();
-      } else {
-        this.counter -= SECOND;
-        this.updateTimeLeftOnPrompt(this.counter/SECOND);
-      }
-    },
+        promptUpdate: function () {
+            if (this.counter === 0) {
+                this.expireSession();
+            } else {
+                this.counter -= SECOND;
+                this.updateTimeLeftOnPrompt(this.counter / SECOND);
+            }
+        },
 
-    togglePromptVisibility: function(){
-      $("#session_prompt").toggleClass("hidden");
-    },
+        togglePromptVisibility: function () {
+            $("#session_prompt").toggleClass("hidden");
+        },
 
-    updateTimeLeftOnPrompt: function(seconds){
-      var mins = ~~(seconds / 60);
-      var secs = seconds % 60;
-      var time = (mins === 0) ? secs : (mins + ":" + this.padSeconds(secs));
-      $('#session_prompt_time_left').text(time);
-    },
+        updateTimeLeftOnPrompt: function (seconds) {
+            var mins = ~~(seconds / 60);
+            var secs = seconds % 60;
+            var time = (mins === 0) ? secs : (mins + ":" + this.padSeconds(secs));
+            $('#session_prompt_time_left').text(time);
+        },
 
-    padSeconds: function(secs){
-      return ((secs + "").length === 1) ? "0" + secs : secs;
-    },
+        padSeconds: function (secs) {
+            return ((secs + "").length === 1) ? "0" + secs : secs;
+        },
 
-    expireSession: function(){
-      location.href = location.protocol + "//" + location.host + "/application/session-expired";
-    },
+        expireSession: function () {
+            location.href = location.protocol + "//" + location.host + "/application/session-expired";
+        },
 
-    refreshSession: function(){
-      clearInterval(this.timerRef);
-      $.ajax({
-        url: "/application/refresh-session"
-      }).done(
-        $.proxy(function(){
-          this.togglePromptVisibility();
-          this.init();
-        }, this)
-      );
-    }
-  }
+        refreshSession: function () {
+            clearInterval(this.timerRef);
+            $.ajax({
+                url: "/application/refresh-session"
+            }).done(
+                $.proxy(function () {
+                    this.togglePromptVisibility();
+                    this.init();
+                }, this)
+            );
+        }
+    };
 
 })();
