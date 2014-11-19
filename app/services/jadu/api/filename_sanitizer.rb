73dab4fbe559075aa62640153eb853287302bf29
Paul Battley <pbattley@gmail.com>
2014-11-19 14:10:03 +0000

    Sanitise filenames to suit the Jadu API

---
+++ app/services/jadu/api/filename_sanitizer.rb
@@ -0,0 +1,83 @@
+module Jadu
+  class API
+    # Usage: FilenameSanitizer.sanitize(string)
+    #
+    # Returns a string modified to fit the constraints of the Jadu API,
+    # namely:
+    #
+    #
+    #   1. To sanitize the filename remove any character not in one of the
+    #      following unicode points.
+    #
+    #      \p{L}   any kind of letter from any language
+    #      \p{N}   any kind of numeric character
+    #      \p{M}   a character intended to be combined with another character
+    #              (e.g. accents, umlauts, enclosing boxes etc...)
+    #      \p{Cf}  invisible formatting indicator
+    #      \p{Zs}  a whitespace character that is invisible, but does take up
+    #              space
+    #
+    #   2. Remove any whitespace from the start and end of the file
+    #
+    #   3. Replace whitespace with underscores
+    #
+    class FilenameSanitizer
+      def self.sanitize(s)
+        new(s).sanitize
+      end
+
+      def initialize(str)
+        @str = str
+      end
+
+      PIPELINE = [
+        :remove_leading_space,
+        :remove_trailing_space,
+        :replace_disallowed_characters_with_underscores,
+        :replace_spaces_with_underscores,
+        :remove_leading_junk,
+        :remove_trailing_junk,
+      ]
+
+      def sanitize
+        name, ext = extract_extension(@str)
+        PIPELINE.inject(name) { |s, m| send(m, s) } + ext
+      end
+
+      private
+
+      def extract_extension(str)
+        match = str.match(/\A(.*?)(\.\p{Alnum}{3})\z/)
+        if match
+          [match[1], match[2]]
+        else
+          [str, '']
+        end
+      end
+
+      def remove_leading_space(s)
+        s.sub(/\A[\p{Space}\p{Cf}]+/, '')
+      end
+
+      def remove_trailing_space(s)
+        s.sub(/[\p{Space}\p{Cf}]+\z/, '')
+      end
+
+      def replace_disallowed_characters_with_underscores(s)
+        s.gsub(/[^\p{Alnum}\p{Mark}\p{Cf}\p{Space}]+/, '_')
+      end
+
+      def replace_spaces_with_underscores(s)
+        s.gsub(/[\p{Space}\p{Cf}]+/, '_')
+      end
+
+      def remove_leading_junk(s)
+        s.sub(/\A\p{^Alnum}+/, '')
+      end
+
+      def remove_trailing_junk(s)
+        s.sub(/\p{^Alnum}+\z/, '')
+      end
+    end
+  end
+end
---
+++ spec/services/jadu/api/filename_sanitizer_spec.rb
@@ -0,0 +1,54 @@
+RSpec.describe Jadu::API::FilenameSanitizer do
+  SPACE = [
+    0x0020, # SPACE
+    0x00A0, # NO-BREAK SPACE
+    0x1680, # OGHAM SPACE MARK
+    0x180E, # MONGOLIAN VOWEL SEPARATOR
+    0x2000, # EN QUAD
+    0x2001, # EM QUAD
+    0x2002, # EN SPACE
+    0x2003, # EM SPACE
+    0x2004, # THREE-PER-EM SPACE
+    0x2005, # FOUR-PER-EM SPACE
+    0x2006, # SIX-PER-EM SPACE
+    0x2007, # FIGURE SPACE
+    0x2008, # PUNCTUATION SPACE
+    0x2009, # THIN SPACE
+    0x200A, # HAIR SPACE
+    0x200B, # ZERO WIDTH SPACE
+    0x202F, # NARROW NO-BREAK SPACE
+    0x205F, # MEDIUM MATHEMATICAL SPACE
+    0x3000, # IDEOGRAPHIC SPACE
+    0xFEFF  # ZERO WIDTH NO-BREAK SPACE
+  ].pack('U*')
+
+  def self.permit(desc, input)
+    it desc do
+      expect(described_class.sanitize(input)).to eql(input)
+    end
+  end
+
+  def self.sanitize(desc, input, output)
+    it desc do
+      expect(described_class.sanitize(input)).to eql(output)
+    end
+  end
+
+  permit 'Latin', 'Latin'
+  permit 'Cyrillic', 'Кириллица'
+  permit 'Greek', 'Ελληνική'
+  permit 'accented letters', 'café'
+  permit 'Chinese', '中文'
+  permit 'Japanese', 'かな'
+  permit 'numbers', '0123456789'
+  permit 'periods', 'example.pdf'
+  permit 'PDF files', 'example.pdf'
+  permit 'CSV files', 'example.csv'
+  permit 'RTF files', 'example.rtf'
+  permit 'text files', 'example.txt'
+
+  sanitize 'leading and trailing space', "#{SPACE}text#{SPACE}", 'text'
+  sanitize 'internal space', "file#{SPACE}with#{SPACE}spaces.pdf", 'file_with_spaces.pdf'
+  sanitize 'punctuation', 'test"+-@#text', 'test_text'
+  sanitize 'additional periods', 'foo.bar.baz.pdf', 'foo_bar_baz.pdf'
+end
