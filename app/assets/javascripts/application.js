678c8366d3dbcf66936c9187a1cac5d1649bc0da
David Plews <david.plews@digital.justice.gov.uk>
2014-11-19 15:21:13 +0000

    Just for testing
    - on a page where the session expiry is implemented type: expire123
    - this will reset the timer down to 15sec to appear and 1:30m to count down
    - refresh the page to clear

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,15 @@ var jqueryPubSub = require('./modules/moj.jquery-pub-sub'),
   selectedOption = require('./modules/moj.selected-option'),
   formHintReveal = require('./modules/moj.reveal-hints'),
   removeMultiple = require('./modules/moj.remove-multiple'),
-  sessionPrompt = require('./modules/moj.session-prompt');
+  sessionPrompt = require('./modules/moj.session-prompt'),
+  cheetJS = require('cheet.js');
+
+cheetJS('e x p i r e 1 2 3', function () {
+  $('#logo img').fadeOut().fadeIn();
+  sessionPrompt.init({
+    FIVE_MINUTES: 90000,
+    FIFTY_FIVE_MINUTES: 5000
+  });
+});
 
 revealPubSub.init();
--- app/assets/javascripts/modules/moj.session-prompt.js
+++ app/assets/javascripts/modules/moj.session-prompt.js
@@ -1,68 +1,71 @@
 // Manages session timeout & displays prompt prior to session expiry
-module.exports = (function() {
+module.exports = (function () {
 
-  var SECOND = 1000,
-      MINUTE = 60 * SECOND,
-      FIVE_MINUTES = 5 * MINUTE,
-      FIFTY_FIVE_MINUTES = 55 * MINUTE;
+  var settings = {
+    SECOND: 1000
+  };
+  settings.MINUTE = 60 * settings.SECOND;
+  settings.FIVE_MINUTES = 5 * settings.MINUTE;
+  settings.FIFTY_FIVE_MINUTES = 55 * settings.MINUTE;
 
   var sessionPrompt = {
 
     timerRef: null,
 
-    init: function(){
-      this.counter = FIVE_MINUTES;
+    init: function (options) {
+      settings = $.extend(settings, options);
+      this.counter = settings.FIVE_MINUTES;
       this.setPromptExtendSessionClickEvent();
       this.startSessionTimer();
     },
 
-    startSessionTimer: function(){
-      setTimeout($.proxy(this.timeoutPrompt, this), FIFTY_FIVE_MINUTES);
+    startSessionTimer: function () {
+      setTimeout($.proxy(this.timeoutPrompt, this), settings.FIFTY_FIVE_MINUTES);
     },
 
-    setPromptExtendSessionClickEvent: function(){
+    setPromptExtendSessionClickEvent: function () {
       $("#session_prompt_continue_btn").unbind().on("click", $.proxy(this.refreshSession, this));
     },
 
-    timeoutPrompt: function(){
-      this.timerRef = setInterval($.proxy(this.promptUpdate, this), SECOND);
+    timeoutPrompt: function () {
+      this.timerRef = setInterval($.proxy(this.promptUpdate, this), settings.SECOND);
       this.togglePromptVisibility();
     },
 
-    promptUpdate: function(){
-      if(this.counter === 0){
+    promptUpdate: function () {
+      if (this.counter === 0) {
         this.expireSession();
       } else {
-        this.counter -= SECOND;
-        this.updateTimeLeftOnPrompt(this.counter/SECOND);
+        this.counter -= settings.SECOND;
+        this.updateTimeLeftOnPrompt(this.counter / settings.SECOND);
       }
     },
 
-    togglePromptVisibility: function(){
+    togglePromptVisibility: function () {
       $("#session_prompt").toggleClass("hidden");
     },
 
-    updateTimeLeftOnPrompt: function(seconds){
+    updateTimeLeftOnPrompt: function (seconds) {
       var mins = ~~(seconds / 60);
       var secs = seconds % 60;
       var time = (mins === 0) ? secs : (mins + ":" + this.padSeconds(secs));
       $('#session_prompt_time_left').text(time);
     },
 
-    padSeconds: function(secs){
+    padSeconds: function (secs) {
       return ((secs + "").length === 1) ? "0" + secs : secs;
     },
 
-    expireSession: function(){
+    expireSession: function () {
       location.href = location.protocol + "//" + location.host + "/application/session-expired";
     },
 
-    refreshSession: function(){
+    refreshSession: function () {
       clearInterval(this.timerRef);
       $.ajax({
         url: "/application/refresh-session"
       }).done(
-        $.proxy(function(){
+        $.proxy(function () {
           this.togglePromptVisibility();
           this.init();
         }, this)
