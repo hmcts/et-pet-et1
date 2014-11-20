17487e6510a35d0b7451317efd5a343a08957931
Paul Battley <pbattley@gmail.com>
2014-11-20 11:37:31 +0000

    Format app references as XXX-XXXX
    * Where the reference would be shorter than seven digits, it's padded
      with leading zeros.
    * Case, formatting, and transcription errors (O for 0 etc.) are handled
      when unobfuscating, so that 1bqhmcm, 1BQ-HMCM, and lBQ HMCM all work.

--- app/services/key_obfuscator.rb
+++ app/services/key_obfuscator.rb
@@ -10,10 +10,28 @@ class KeyObfuscator
   end
 
   def obfuscate(numeric)
-    Base32::Crockford.encode(@obfuscator.obfuscate(numeric))
+    format(encode(@obfuscator.obfuscate(numeric)))
   end
 
   def unobfuscate(string)
-    @obfuscator.unobfuscate(Base32::Crockford.decode(string).unpack('N')[0])
+    @obfuscator.unobfuscate(decode(clean(string)))
+  end
+
+  private
+
+  def encode(number)
+    Base32::Crockford.encode(number)
+  end
+
+  def decode(string)
+    Base32::Crockford.decode(string).unpack('N')[0]
+  end
+
+  def format(digits)
+    digits.rjust(7, '0').scan(/\A.{3}|.{4}/).join('-')
+  end
+
+  def clean(string)
+    string.upcase.gsub(/\A[0O]|[^0-9A-Z]/, '')
   end
 end
--- spec/services/key_obfuscator_spec.rb
+++ spec/services/key_obfuscator_spec.rb
@@ -6,14 +6,36 @@ RSpec.describe KeyObfuscator, type: :service do
   end
 
   describe '#obfuscate' do
-    it 'returns an obfuscated representation of the input' do
-      expect(subject.obfuscate 1).to eq('1BQHMCM')
+    it 'returns an obfuscated representation of the input in the format 3-4' do
+      expect(subject.obfuscate(1)).to eq('1BQ-HMCM')
+    end
+
+    it 'adds leading zeros to make seven characters' do
+      expect(subject.obfuscate(4285926522)).to eq("02Z-PPNV")
     end
   end
 
   describe '#unobfuscate' do
     it 'returns the input unobfuscated' do
-      expect(subject.unobfuscate '1BQHMCM').to eq(1)
+      expect(subject.unobfuscate('1BQ-HMCM')).to eq(1)
+    end
+
+    it 'ignores a missing hyphen' do
+      expect(subject.unobfuscate('1BQHMCM')).to eq(1)
+    end
+
+    it 'ignores extra spaces' do
+      expect(subject.unobfuscate('1BQ HMCM')).to eq(1)
+    end
+
+    it 'interprets ambiguous characters' do
+      expect(subject.unobfuscate('LBQ-HMCM')).to eq(1)
+      expect(subject.unobfuscate('IBQ-HMCM')).to eq(1)
+      expect(subject.unobfuscate('O2Z-PPNV')).to eq(4285926522)
+    end
+
+    it 'is case-insensitive' do
+      expect(subject.unobfuscate('1bq hmcm')).to eq(1)
     end
   end
 end
