25633ffd89163bc723985a74363f0e07b9eec85c
David Plews <david.plews@digital.justice.gov.uk>
2014-11-25 17:27:24 +0000

    Containers for the HTML
    - Copy is being done ATM

--- spec/mailer_previews/base_preview.rb
+++ spec/mailer_previews/base_preview.rb
@@ -1,4 +1,20 @@
 class BaseMailerPreview < ActionMailer::Preview
+  def started_application
+    BaseMailer.started_application(Claim.last)
+  end
+
+  def submitted_application
+    BaseMailer.submitted_application(Claim.last)
+  end
+
+  def unsuccessful_application
+    BaseMailer.unsuccessful_application(Claim.last)
+  end
+
+  def remission
+    BaseMailer.remission(Claim.last)
+  end
+
   def access_details_email
     BaseMailer.access_details_email(Claim.last)
   end
