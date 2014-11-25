dd62c3b48d76bada3997d8a8d7838e3677e4964f
David Plews <david.plews@digital.justice.gov.uk>
2014-11-25 17:27:24 +0000

    Containers for the HTML
    - Copy is being done ATM

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -7,6 +7,30 @@ class BaseMailer < ActionMailer::Base
       subject: t('base_mailer.access_details_email.subject', reference: @claim.reference))
   end
 
+  def started_application(claim)
+    @claim = claim
+    mail(to: @claim.email_address,
+      subject: t('base_mailer.started_application.subject', reference: @claim.reference))
+  end
+
+  def submitted_application(claim)
+    @claim = claim
+    mail(to: @claim.email_address,
+      subject: t('base_mailer.submitted_application.subject', reference: @claim.reference))
+  end
+
+  def unsuccessful_application(claim)
+    @claim = claim
+    mail(to: @claim.email_address,
+      subject: t('base_mailer.unsuccessful_application.subject', reference: @claim.reference))
+  end
+
+  def remission(claim)
+    @claim = claim
+    mail(to: @claim.email_address,
+      subject: t('base_mailer.remission.subject', reference: @claim.reference))
+  end
+
   def confirmation_email(claim, email_addresses)
     @claim = claim
     pdf_form = PdfFormBuilder.new(@claim)
