029c8a48a98495ac62a72faef974003d3109614c
David Plews <david.plews@digital.justice.gov.uk>
2014-11-27 17:12:46 +0000

    refactor ton CSS and JS
    - arrow now working

--- app/assets/javascripts/modules/moj.state-manager.js
+++ app/assets/javascripts/modules/moj.state-manager.js
@@ -2,9 +2,11 @@ module.exports = (function () {
 
   $(function(){
     $('#navcontainer').on('click','.menu-trigger', function (e) {
-      var el = $(e.currentTarget);
+      var el = $(e.currentTarget),
+          $el = $(el);
       // show the right one.
-      $(el).next().toggleClass('visuallyhidden');
+      $el.find('span').toggleClass('arrow-down');
+      $el.next().toggleClass('visuallyhidden');
     });
   });
 
