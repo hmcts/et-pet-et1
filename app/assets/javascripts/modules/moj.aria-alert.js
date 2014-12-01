4782762b260fbc81c96be6bf7cbaf622cd358a7d
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2014-12-01 15:28:04 +0000

    updated aria message

--- app/assets/javascripts/modules/moj.aria-alert.js
+++ app/assets/javascripts/modules/moj.aria-alert.js
@@ -1,8 +1,18 @@
 // Focuses on aria alert if it's in the page for screen readers
 
 module.exports = (function() {
-  var ariaAlert = document.getElementById('error-summary');
-  if(ariaAlert) {
-    ariaAlert.focus();
-  }
+
+  var aria = {};
+
+  aria.init = function() {
+    var ariaAlert = document.getElementById('error-summary');
+    if(ariaAlert) {
+      ariaAlert.focus();
+      ariaAlert.scrollIntoView();
+    }
+  };
+
+  aria.init();
+
+  return aria;
 })();
