daaa94713a4937e333dc2828c5a1282162474563
Stevie Graham <sjtgraham@mac.com>
2014-12-02 15:15:07 +0000

    Use BarclayCard test gateway on all hosts except where ENV == 'prod'

--- config/initializers/barclaycard.rb
+++ config/initializers/barclaycard.rb
@@ -6,4 +6,4 @@ end
 
 EPDQ.config pspid: ENV.fetch('EPDQ_PSPID'), sha_type: :sha256,
   sha_in: ENV.fetch('EPDQ_SECRET_IN'), sha_out: ENV.fetch('EPDQ_SECRET_OUT'),
-  test_mode: !Rails.env.production?
+  test_mode: ENV['ENV'] == 'prod' ? false : true
