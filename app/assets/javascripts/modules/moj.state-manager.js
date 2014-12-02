6ff1708f4c66cd3f1218bd65a308b668c325809a
David Plews <david.plews@digital.justice.gov.uk>
2014-12-02 10:27:08 +0000

    removed messedup selector

--- app/assets/javascripts/modules/moj.state-manager.js
+++ app/assets/javascripts/modules/moj.state-manager.js
@@ -2,9 +2,7 @@ module.exports = (function () {
 
   $(function(){
     $('#navcontainer').on('click','.menu-trigger', function (e) {
-      var el = $(e.currentTarget),
-          $el = $(el);
-      // show the right one.
+      var $el = $(e.currentTarget);
       $el.find('span').toggleClass('arrow-down');
       $el.next().toggleClass('visuallyhidden');
     });
