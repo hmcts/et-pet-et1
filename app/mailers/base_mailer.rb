7d71810247434ac4ffdedebf0418cb3232a15d21
Paul Battley <pbattley@gmail.com>
2014-12-05 17:01:35 +0000

    Remove unused emails
    These are different views of the confirmation email, which now
    incorporates these changes.

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -7,24 +7,6 @@ class BaseMailer < ActionMailer::Base
       subject: t('base_mailer.access_details_email.subject', reference: @claim.reference))
   end
 
-  def submitted_application(claim)
-    @claim = claim
-    mail(to: @claim.email_address,
-      subject: t('base_mailer.submitted_application.subject', reference: @claim.reference))
-  end
-
-  def unsuccessful_application(claim)
-    @claim = claim
-    mail(to: @claim.email_address,
-      subject: t('base_mailer.unsuccessful_application.subject', reference: @claim.reference))
-  end
-
-  def remission(claim)
-    @claim = claim
-    mail(to: @claim.email_address,
-      subject: t('base_mailer.remission.subject', reference: @claim.reference))
-  end
-
   def confirmation_email(claim, email_addresses)
     @claim = claim
     @claim.generate_pdf!
--- spec/mailer_previews/base_preview.rb
+++ spec/mailer_previews/base_preview.rb
@@ -1,16 +1,4 @@
 class BaseMailerPreview < ActionMailer::Preview
-  def submitted_application
-    BaseMailer.submitted_application(Claim.last)
-  end
-
-  def unsuccessful_application
-    BaseMailer.unsuccessful_application(Claim.last)
-  end
-
-  def remission
-    BaseMailer.remission(Claim.last)
-  end
-
   def access_details_email
     BaseMailer.access_details_email(Claim.last)
   end
