b5f3ddbf7c8e3a95379511077bee52e41c66f044
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2014-12-05 11:26:45 +0000

    multiplesJS now allows infinite multiples

--- app/assets/javascripts/modules/moj.remove-multiple.js
+++ app/assets/javascripts/modules/moj.remove-multiple.js
@@ -9,7 +9,7 @@ module.exports = (function() {
     multiples.each(function(i, el) {
       var checkbox = $(el).find('.destroy-checkbox');
       //bind the remove method
-      removeMultiple.bindRemoveButton(el, checkbox);
+      removeMultiple.setRemoveButton(el, checkbox);
       // hide if already checked
       el.style.display = checkbox.is(':checked') ? 'none' : 'block';
     });
@@ -17,7 +17,7 @@ module.exports = (function() {
     removeMultiple.updateCount();
   };
 
-  removeMultiple.bindRemoveButton = function(el, checkbox) {
+  removeMultiple.setRemoveButton = function(el, checkbox) {
     var link = $(el).find('.remove-multiple');
 
     //hide on click and check the hidden checkbox
@@ -34,17 +34,17 @@ module.exports = (function() {
 
     // only update the counter on visible multiples
     visibles.each(function(i, el) {
+      //incremented by 2 because the first is the main claimant
       var section = $(el),
         count = i + 2;
       // update the string for the user
-      replaceNumber(section.find('legend:first'), count);
-      //replaceNumber(section.find('.remove-multiple'), count);
+      replaceCount(section.find('legend:first'), count);
     });
 
-    function replaceNumber(el, count) {
-      return $(el).text(function() {
-        // replace the last character which we know to be a number.
-        return $(el).text().slice(0,-1) + count;
+    function replaceCount(el, count) {
+      return $(el).text(function()
+        // find the last occurrence of a number in the string and replace it
+        return $(el).text().replace(/\d+\Z/, count);
       });
     }
   };
