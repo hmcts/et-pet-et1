e4f133ee5b11f88b895c4b349e0ef521c204a77c
mattmb <matthew.mead-briggs@digital.justice.gov.uk>
2014-12-11 12:17:12 +0000

    We need the = here or the config is not set. Also carrierwave expects AWS not aws.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage                          :fog
-    config.fog_public                       false
-    config.fog_directory                    ENV.fetch('S3_UPLOAD_BUCKET')
-    config.fog_authenticated_url_expiration ENV.fetch('S3_URL_EXPIRY')
+    config.fog_public                       = false
+    config.fog_directory                    = ENV.fetch('S3_UPLOAD_BUCKET')
+    config.fog_authenticated_url_expiration = ENV.fetch('S3_URL_EXPIRY')
     config.fog_credentials = {
-      provider:              'aws',
+      provider:              'AWS',
       region:                'eu-west-1',
       aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
       aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
