0bf46f10c6173fdb68402b9dcf802c8bc601fda6
David Plews <david.plews@digital.justice.gov.uk>
2014-12-18 16:01:56 +0000

    Updates: JS, Aside partial
    - new methods for errors on pages
    - implemented data tags on aside page
    - WIP

--- app/assets/javascripts/modules/moj.ga-tracker.js
+++ app/assets/javascripts/modules/moj.ga-tracker.js
@@ -1,29 +1,37 @@
-module.exports = (function($) {
+module.exports = (function ($) {
 
   var gaTracker = {
-    debug: false
+    error: {
+      type: 'event',
+      label: 'error'
+    }
   };
 
-  gaTracker.init = function() {
+  gaTracker.init = function () {
     gaTracker.bindListners();
+    gaTracker.errorHook();
   };
 
   gaTracker.bindListners = function () {
-    $('[data-ga-tracking]').on('click', function(e){
-      if(gaTracker.debug){
-        e.preventDefault();
-      }
-      var data = {
-        eventType: $(e.currentTarget).data('ga-tracking-event'),
-        url: $(e.currentTarget).data('ga-tracking-url')
-      };
+    $('[data-ga-tracking-type]').on('click', function (e) {
+      var $el = $(e.currentTarget),
+        data = {
+          type: $el.data('ga-type'),
+          label: $el.data('ga-label')
+        };
       gaTracker.gaProxy(data);
     });
   };
 
+  gaTracker.errorHook = function () {
+    $('#error-summary').is(function () {
+      gaTracker.gaProxy(gaTracker.error);
+    });
+  };
+
   gaTracker.gaProxy = function (data) {
-    if(ga){
-      ga('send', data.eventType, data.url);
+    if (ga) {
+      ga('send', data.type, data.label);
     }
   };
 
