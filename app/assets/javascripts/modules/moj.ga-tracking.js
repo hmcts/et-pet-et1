4730ec323c936a38519433502695f62dd73fb3e6
David Plews <david.plews@digital.justice.gov.uk>
2015-01-15 14:23:34 +0000

    Module: moj.ga-tracking
    The module does:
     - Regular page views
     - Custom Event tracking
     - Virtual page views tracking

--- app/assets/javascripts/modules/moj.ga-tracking.js
+++ app/assets/javascripts/modules/moj.ga-tracking.js
@@ -1,59 +1,176 @@
 module.exports = (function () {
-
+  /**
+   * Loading the GA JS.
+   */
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  var gaTracking = {
-    error: {
-      type: 'event',
-      label: 'error'
+  var tpl, utils, gaTracking;
+
+  /**
+   * Blue prints for Analytics Field objects
+   * @type {Object}
+   */
+  tpl = {
+    events: {
+      'hitType': 'event', // Required - always ‘event’
+      'eventAction': 'click', // Required - always ‘click’
+      'eventCategory': '__default__', // Required - either ‘button;, ‘radio’ or ‘link’
+      'eventLabel': '__default__' // Required (for us) - value from attribute or innerHTML
+    }
+  };
+
+  // Util methods
+  utils = {
+    pageURL: window.location.pathname + window.location.search,
+    /**
+     * Extract the correct values from different types of elements
+     * using the tagName property of the element.
+     */
+    events: {
+      A: function (elm) {
+        return {
+          eventCategory: 'link',
+          eventLabel: elm.innerHTML
+        };
+      },
+      INPUT: function (elm) {
+        return {
+          eventCategory: elm.type,
+          eventLabel: (elm.type === 'submit' ? elm.value : elm.id)
+        };
+      }
     },
+
+    /**
+     * Return the field object to pass to the `ga` function
+     * with the correct data extracted from the DOM
+     * @param  { HTMLElement }  elm
+     * @return { Object }
+     */
+    getEventsDataObj: function (elm) {
+      return $.extend({}, tpl.events, utils.events[elm.tagName](elm));
+    },
+
+    /**
+     * Return the field object to pass to the `ga` function
+     * with the correct data extracted from the DOM
+     * @param  { HTMLElement } elm
+     * @return { Object }
+     */
+    getPageViewDataObj: function (elm) {
+      return {
+        'hitType': 'pageview',
+        'page': 'reveal-' + elm.id,
+        'title': 'reveal'
+      };
+    },
+
+    /**
+     * Check for the `ga` method and Analytics ID
+     * @return { boolean }
+     */
+    sendGaRequest: function () {
+      return (gaTracking.gaID && typeof ga === 'function');
+    },
+
+    /**
+     * Closure to bind click events
+     * @param  { jQuery     HMTLElement } $elm
+     * @param  { object }   gaData        Analytics Field Object
+     */
+    clickEvent: function ($elm, gaData) {
+      return (function () {
+        $elm.on('click', function () {
+          gaTracking.gaProxy(gaData);
+        });
+      }());
+    }
+  };
+
+  /**
+   * Internal object
+   * @type {Object}
+   */
+  gaTracking = {
     gaID: null
   };
 
+  /**
+   * Init method.
+   */
   gaTracking.init = function () {
     gaTracking.pageView();
     gaTracking.errorMessageCheck();
   };
 
-  gaTracking.gaProxy = function (data) {
-    if (ga) {
-      ga('send', data.type, data.label);
+  /**
+   * Proxy to Analytics call for custom tracking
+   * @param  { object } gaData  Analytics Field Object
+   */
+  gaTracking.gaProxy = function (gaData) {
+    if (utils.sendGaRequest()) {
+      ga('send', gaData);
     }
   };
 
+  /**
+   * Analytics page view calls
+   */
   gaTracking.pageView = function () {
-    if (gaTracking.gaID) {
+    if (utils.sendGaRequest()) {
       ga('create', gaTracking.gaID, 'auto');
+
+      if (utils.pageURL.indexOf('/apply/pay/') !== -1) {
+        ga('set', 'referrer', ' ');
+      }
+
       ga('send', 'pageview');
       ga('set', 'anonymizeIP', true);
+
     }
   };
 
-  gaTracking.bindListener = (function () {
-    var $el, data;
-    $('[data-ga-type]').on('click', function (e) {
-      $el = $(e.currentTarget);
-      if (!$el.data('hasClicked')) {
-        data = {
-          type: $el.data('ga-type'),
-          label: $el.data('ga-label')
-        };
-
-        $el.data('hasClicked', true);
-        gaTracker.gaProxy(data);
-      }
-    });
-  })();
-
+  /**
+   * Handle the virtual page view when the page has errors
+   */
   gaTracking.errorMessageCheck = function () {
-    $('#error-summary').is(function () {
-      gaTracking.gaProxy(gaTracking.error);
+    $('#error-summary').is(function (idx, elm) {
+      var gaData = {
+        'hitType': 'pageview',
+        'page': 'error-' + utils.pageURL,
+        'title': 'error'
+      };
+      gaTracking.gaProxy(gaData);
     });
   };
 
+  /**
+   * Method to handle Virtual Page Views on click
+   */
+  gaTracking.gaVirtualPageViews = (function () {
+    $('.ga-vpv').each(function (idx, elm) {
+      var $elm = $(elm);
+      utils.clickEvent($elm, utils.getPageViewDataObj(elm));
+    });
+  }());
+
+  /**
+   * Method to handle Custom Events on click
+   */
+  gaTracking.gaCustomEvents = (function () {
+    $('.ga-event').each(function (idx, elm) {
+      var $elm = $(elm);
+      utils.clickEvent($elm, utils.getEventsDataObj(elm));
+    });
+  }());
+
+  /**
+   * Return an init method to allow the Google Analytics ID
+   * to be set from the View
+   */
   return {
     init: function (gaID) {
       gaTracking.gaID = gaID;
