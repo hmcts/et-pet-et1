325e4b3110a3904d2310e42dfc6690189e2a4c51
Stevie Graham <sjtgraham@mac.com>
2015-01-26 14:58:01 +0000

    Flash messages  - Add flash messages for info, notice, and error  - Add logout message

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -31,7 +31,7 @@ class UserSessionsController < ApplicationController
   def logout
     claim.create_event Event::LOGOUT, actor: 'user'
     reset_session
-    redirect_to root_path
+    redirect_to root_path, notice: t('.logout')
   end
 
   def send_access_details_and_logout
