2995e0db69f4e495f6adb88a96efa55a30740da7
Dan Palmer <boss@bitrecursive.com>
2015-02-01 18:34:15 +0000

    Increases the DX length for Representatives from 20 to 40

--- app/forms/representative_form.rb
+++ app/forms/representative_form.rb
@@ -19,7 +19,7 @@ class RepresentativeForm < Form
   validates :type, :name, presence: true
   validates :type, inclusion: { in: TYPES }
   validates :organisation_name, :name, length: { maximum: 100 }
-  validates :dx_number, length: { maximum: 20 }
+  validates :dx_number, length: { maximum: 40 }
   validates :mobile_number, length: { maximum: PHONE_NUMBER_LENGTH }
 
   def valid?
