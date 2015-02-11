b7135704504ea3a2251fa2c5869be627cc8be90b
mattmb <matthew.mead-briggs@digital.justice.gov.uk>
2015-02-11 16:18:20 +0000

    Enable connection pooling for the sneakers workers and reconect after fork. This prevents us creating too many physical TCP connections to the database and hitting a hard limit set on the DB instance.

--- config/initializers/amqp.rb
+++ config/initializers/amqp.rb
@@ -9,7 +9,19 @@ if Rails.env.production?
     log:     'log/sneakers.log',
     timeout_job_after: ENV.fetch('SNEAKERS_TIMEOUT_S').to_i,
     retry_timeout: ENV.fetch('SNEAKERS_RETRY_TTL_MS').to_i, #milliseconds
-    max_retries: ENV.fetch('SNEAKERS_MAX_RETRIES').to_i
+    max_retries: ENV.fetch('SNEAKERS_MAX_RETRIES').to_i,
+    hooks: {
+        before_fork: -> {
+           Sneakers::logger.info('Worker: Disconnect from the database')
+           ActiveRecord::Base.connection_pool.disconnect!
+        },
+        after_fork: -> {
+          config = Rails.application.config.database_configuration[Rails.env]
+          config['pool'] = ENV['SNEAKERS_DB_POOL'] || 1 #Limit the number of TCP connections per worker
+          ActiveRecord::Base.establish_connection(config)
+          Sneakers::logger.info('Worker: Reconnect to the database')
+        }
+    }
 
   Rails.application.config.active_job.queue_adapter = :sneakers
 end
