584f8feb40e1d253eeaa8b3fbffe3e1ff9bc224a
Dan Palmer <boss@bitrecursive.com>
2015-02-17 00:40:20 +0000

    Don’t mixin, just point to the modules constant.

--- app/forms/representative_form.rb
+++ app/forms/representative_form.rb
@@ -1,5 +1,4 @@
 class RepresentativeForm < Form
-  include RepresentativeType
   include AddressAttributes
 
   attribute :type,               String
@@ -15,7 +14,7 @@ class RepresentativeForm < Form
   before_validation :destroy_target!, unless: :has_representative?
 
   validates :type, :name, presence: true
-  validates :type, inclusion: { in: TYPES }
+  validates :type, inclusion: { in: RepresentativeType::TYPES }
   validates :organisation_name, :name, length: { maximum: 100 }
   validates :dx_number, length: { maximum: 40 }
   validates :mobile_number, length: { maximum: PHONE_NUMBER_LENGTH }
