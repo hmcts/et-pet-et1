74a1d223e3a34f1c8aa02c241ef195a7d462e05a
Stevie Graham <sjtgraham@mac.com>
2015-02-20 16:55:59 +0000

    Revert "Adds representative type to XML - SUCH AVOID MERGING"

--- app/forms/representative_form.rb
+++ app/forms/representative_form.rb
@@ -1,4 +1,7 @@
 class RepresentativeForm < Form
+  TYPES = %w<citizen_advice_bureau free_representation_unit
+    law_centre trade_union solicitor private_individual trade_association other>.freeze
+
   include AddressAttributes
 
   attribute :type,               String
@@ -14,7 +17,7 @@ class RepresentativeForm < Form
   before_validation :destroy_target!, unless: :has_representative?
 
   validates :type, :name, presence: true
-  validates :type, inclusion: { in: RepresentativeType::TYPES }
+  validates :type, inclusion: { in: TYPES }
   validates :organisation_name, :name, length: { maximum: 100 }
   validates :dx_number, length: { maximum: 40 }
   validates :mobile_number, length: { maximum: PHONE_NUMBER_LENGTH }
