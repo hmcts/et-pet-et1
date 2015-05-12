f696ed6d357913f9a6a956f9b8dd74cca2b6a7bf
Dan Palmer <boss@bitrecursive.com>
2015-05-12 14:48:01 +0100

    Returning to the form from the guide page takes the user to the last form page they visited

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,16 @@
 class GuidesController < MarkdownController
   add_markdown_path   Rails.root.join(*%w<app views guides markdown>)
   add_markdown_files  %w<time_limits acas_early_conciliation acas_early_conciliation_exceptions fees_and_payment fees_and_payment_working_out_your_fee fees_and_payment_paying_your_fee reducing_fees writing_your_claim_statement>
+
+  delegate :referrer, :host, to: :request
+
+  def return_to_form
+    if referrer && host == URI.parse(referrer).host
+      referrer
+    else
+      apply_path
+    end
+  end
+
+  helper_method :return_to_form
 end
