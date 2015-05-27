2537ec7854344845d5bf6351580f611e04421bf9
Dan Palmer <boss@bitrecursive.com>
2015-05-27 15:47:00 +0100

    Adds a dashed line break for text based emails

---
+++ spec/presenters/confirmation_email_presenter_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ConfirmationEmailPresenter, type: :presenter do
+  subject { described_class.new Claim.create }
+
+  describe '#text_email_line_break' do
+    it "provides a a consecutive sequence of fifty '=' to form a line break" do
+      expect(subject.text_email_line_break). to match(/^[=]{50}$/)
+    end
+  end
+end
