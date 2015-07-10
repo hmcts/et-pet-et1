7ee192213b94c0a7b9d8badc07b3e7890bc18286
Dan Palmer <boss@bitrecursive.com>
2015-07-10 20:29:08 +0100

    Refactor sendgrid healthcheck component. Use constants for smtp settings.

--- spec/services/healthcheck_component/sengrid_spec.rb
+++ spec/services/healthcheck_component/sengrid_spec.rb
@@ -4,15 +4,6 @@ RSpec.describe HealthcheckComponent::Sendgrid, type: :service do
 
   it_behaves_like 'a healthcheck component'
 
-  let(:spoof_smtp_settings) do
-    { address: 'emailz.example.biz', port: '587' }
-  end
-
-  before(:each) do
-    allow(ActionMailer::Base).to receive(:smtp_settings).
-      and_return(spoof_smtp_settings)
-  end
-
   subject { described_class.new }
 
   context 'sengrid is available' do
@@ -22,7 +13,7 @@ RSpec.describe HealthcheckComponent::Sendgrid, type: :service do
 
     before do
       allow(Net::SMTP).to receive(:start).
-        with('emailz.example.biz', '587').
+        with('localhost', 25).
         and_return(successful_smtp_response)
     end
 
@@ -36,7 +27,7 @@ RSpec.describe HealthcheckComponent::Sendgrid, type: :service do
 
     before do
       allow(Net::SMTP).to receive(:start).
-        with('emailz.example.biz', '587').
+        with('localhost', 25).
         and_return(failed_smtp_response)
     end
 
