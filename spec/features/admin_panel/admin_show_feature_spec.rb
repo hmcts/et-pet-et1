e742329064d7d358d5bd2f5831f8413c0702ac62
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2017-01-17 12:20:25 +0000

    Fix failing tests due to change of event ordering
    Four tests were failing because of a change of the default ordering of
    the Event model

--- spec/features/admin_panel/admin_show_feature_spec.rb
+++ spec/features/admin_panel/admin_show_feature_spec.rb
@@ -130,7 +130,7 @@ RSpec.feature 'Viewing a claims details in the admin interface', type: :feature 
     click_button 'Mark as submitted'
     expect(enqueued_claim.reload.state).to eq 'submitted'
 
-    event = enqueued_claim.events.last
+    event = enqueued_claim.events.reload.last
 
     expect(event.claim_state).to eq 'submitted'
     expect(event.event).to eq Event::MANUAL_STATUS_CHANGE
@@ -145,8 +145,7 @@ RSpec.feature 'Viewing a claims details in the admin interface', type: :feature 
 
     click_button 'Submit claim'
 
-    event = enqueued_claim.events.last
-
+    event = enqueued_claim.events.reload.last
     expect(event.event).to eq Event::MANUALLY_SUBMITTED
     expect(event.actor).to eq 'admin'
   end
