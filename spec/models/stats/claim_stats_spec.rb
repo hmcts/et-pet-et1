dd4f05dbc4383f1755c8a6c91c5f458a08c98316
Petr Zaparka <petr@zaparka.cz>
2017-01-27 15:51:03 +0000

    Replacing timecop with timehelper method.

--- spec/models/stats/claim_stats_spec.rb
+++ spec/models/stats/claim_stats_spec.rb
@@ -3,16 +3,14 @@ require 'rails_helper'
 RSpec.describe Stats::ClaimStats, type: :model do
 
   describe 'scopes' do
-    # before do
-    #   # making sure that we are not in frozen state
-    #   Timecop.return
-    #   Timecop.freeze(current_time)
-    # end
-    # after { Timecop.return }
+    before do
+      travel_to(current_time)
+    end
+    after { travel_back }
 
     subject { described_class }
 
-    let!(:current_time)                     { Time.parse("2017-01-27 13:56:25") }
+    let!(:current_time)                     { Time.parse("2010-01-27 13:56:25") }
     let!(:started_claim)                    { create :claim, :not_submitted }
     let!(:old_started_claim)                { create :claim, :not_submitted, created_at: 91.days.ago }
     let!(:old_out_of_range_started_claim)   { create :claim, :not_submitted, created_at: 92.days.ago }
@@ -24,12 +22,6 @@ RSpec.describe Stats::ClaimStats, type: :model do
     describe '.started_within_max_submission_timeframe' do
       it 'returns claims started within the past 91 days' do
         results = subject.started_within_max_submission_timeframe
-        puts Time.now
-        if results.reload.size < 2
-          puts subject.all.map(&:created_at)
-          puts "---"
-          puts results.map(&:created_at)
-        end
         expect(results.reload.size).to eq 2
 
         query_result_record = results.first
