cfb057f94a87a1b9132614b00887159d4092706f
Petr Zaparka <petr@zaparka.cz>
2017-01-27 14:46:08 +0000

    Checking if Timecop is working.

--- spec/models/stats/claim_stats_spec.rb
+++ spec/models/stats/claim_stats_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Stats::ClaimStats, type: :model do
 
   describe 'scopes' do
-    before { Timecop.freeze(current_time) }
-    after { Timecop.return }
+    # before { Timecop.freeze(current_time) }
+    # after { Timecop.return }
 
     subject { described_class }
 
@@ -20,7 +20,7 @@ RSpec.describe Stats::ClaimStats, type: :model do
     describe '.started_within_max_submission_timeframe' do
       it 'returns claims started within the past 91 days' do
         results = subject.started_within_max_submission_timeframe
-        puts Time.now
+        # puts Time.now
         if results.reload.size < 2
           puts subject.all.map(&:created_at)
           puts "---"
@@ -35,16 +35,12 @@ RSpec.describe Stats::ClaimStats, type: :model do
 
     describe '.completed_within_max_submission_timeframe' do
       it 'returns claims completed within the past 91 days' do
-        Timecop.freeze(current_time) do
-          puts Time.now
-          puts 'completed_within_max_submission_timeframe'
-          expect(Claim.count).to eql(7)
-          results = subject.completed_within_max_submission_timeframe
-          expect(results.size).to eq 2
+        expect(Claim.count).to eql(7)
+        results = subject.completed_within_max_submission_timeframe
+        expect(results.size).to eq 2
 
-          query_result_record = results.first
-          expect(query_result_record.reference).to eq completed_claim.reference
-        end
+        query_result_record = results.first
+        expect(query_result_record.reference).to eq completed_claim.reference
       end
     end
   end
@@ -62,6 +58,27 @@ RSpec.describe Stats::ClaimStats, type: :model do
       expect(described_class).
         to receive_message_chain('started_within_max_submission_timeframe.count') { 5 }
       expect(described_class.started_count).to eq 5
+    end
+  end
+
+  describe 'timecop test' do
+    it "time local" do
+      Timecop.freeze( Time.local(1990, 1, 14, 10, 30)) do
+        expect(Time.now.to_s(:db)).to eql('1990-01-14 10:30:00')
+      end
+    end
+
+    it "time parse" do
+      Timecop.freeze(Time.parse("1995-01-27 13:56:25")) do
+        expect(Time.now.to_s(:db)).to eql('1995-01-27 13:56:25')
+      end
+    end
+
+    it "time local" do
+      t = Time.local(2008, 9, 1, 10, 5, 0)
+      Timecop.travel(t)
+      sleep 3
+      expect(Time.now.to_s(:db)).to eql('2008-09-01 10:05:03')
     end
   end
 end
