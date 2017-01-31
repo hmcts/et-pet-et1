46c96ee5cee5a7c00513cf386d6da9656830fd50
Petr Zaparka <petr@zaparka.cz>
2017-01-31 13:46:25 +0000

    Reverting sensitive - Fees Review Info

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,6 +1,6 @@
 class GuidesController < MarkdownController
   add_markdown_path   Rails.root.join(*%w<app views guides markdown>)
-  add_markdown_files  %w<time_limits acas_early_conciliation acas_early_conciliation_exceptions fees_and_payment fees_and_payment_working_out_your_fee fees_and_payment_paying_your_fee reducing_fees writing_your_claim_statement>
+  add_markdown_files  %w<payments_from_ni_fund time_limits acas_early_conciliation acas_early_conciliation_exceptions fees_and_payment fees_and_payment_working_out_your_fee fees_and_payment_paying_your_fee reducing_fees writing_your_claim_statement>
 
   delegate :referrer, :host, to: :request
 
