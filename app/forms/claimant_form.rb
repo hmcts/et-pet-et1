26cb9cad529de160069a199d98b8227038da4edf
Petr Zaparka <petr@zaparka.cz>
2017-02-08 12:58:47 +0000

    Using age validator that is same for groups

--- app/forms/claimant_form.rb
+++ app/forms/claimant_form.rb
@@ -7,6 +7,7 @@ class ClaimantForm < Form
   NAME_LENGTH          = 100
 
   include AddressAttributes.but_skip_postcode_validation
+  include AgeValidator
 
   validates :address_post_code,
     post_code: true, length: { maximum: POSTCODE_LENGTH },
@@ -74,14 +75,5 @@ class ClaimantForm < Form
 
   def reset_special_needs!
     self.special_needs = nil
-  end
-
-  def older_then_16
-    return if self.date_of_birth.blank? || !self.date_of_birth.is_a?(Date)
-
-    if self.date_of_birth.to_datetime.to_i >= 16.years.ago.to_i
-      message = I18n.t('activemodel.errors.models.claimant.attributes.date_of_birth.too_young')
-      errors.add(:date_of_birth, message)
-    end
   end
 end
