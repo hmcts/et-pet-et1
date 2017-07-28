957bf18b597df9e4c328aaa6f5e89eeaaa32517e
Petr Zaparka <petr@zaparka.cz>
2017-07-28 13:48:00 +0100

    Removing fee reated text from guides

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,12 +1,9 @@
 class GuidesController < MarkdownController
   add_markdown_path views_guides_markdowdon_link
   add_markdown_files %w[
-    payments_from_ni_fund
     time_limits acas_early_conciliation
     acas_early_conciliation_exceptions
-    fees_and_payment fees_and_payment_working_out_your_fee
-    fees_and_payment_paying_your_fee
-    reducing_fees writing_your_claim_statement
+    writing_your_claim_statement
   ]
 
   delegate :referrer, :host, to: :request
--- spec/features/guide_spec.rb
+++ spec/features/guide_spec.rb
@@ -7,10 +7,6 @@ feature 'Guide', type: :feature do
     let(:time_limits_header)          { '<h2>Time limits</h2>' }
     let(:acas_header)                 { '<h2>Acas: early conciliation</h2>' }
     let(:acas_exceptions_header)      { '<h3>Exceptions to early conciliation</h3>' }
-    let(:fees_header)                 { '<h2>Fees and payment</h2>' }
-    let(:working_out_your_fee_header) { '<h3>Working out your fee</h3>' }
-    let(:paying_your_fee_header)      { '<h3>Paying your issue fee</h3>' }
-    let(:reducing_your_fee_header)    { '<h2>Help with fees</h2>' }
     let(:writing_yours_header)        { '<h2>Writing your claim statement</h2>' }
 
     before :each do
@@ -21,29 +17,17 @@ feature 'Guide', type: :feature do
       expect(page.html).to include(time_limits_header)
       expect(page.html).to include(acas_header)
       expect(page.html).to include(acas_exceptions_header)
-      expect(page.html).to include(fees_header)
-      expect(page.html).to include(working_out_your_fee_header)
-      expect(page.html).to include(paying_your_fee_header)
-      expect(page.html).to include(reducing_your_fee_header)
       expect(page.html).to include(writing_yours_header)
     end
 
     let(:time_limits_link)          { "Time limits" }
     let(:acas_link)                 { "Acas: early conciliation" }
     let(:acas_exceptions_link)      { "Exceptions to early conciliation" }
-    let(:fees_link)                 { "Fees and payment" }
-    let(:working_out_your_fee_link) { "Working out your fee" }
-    let(:paying_your_fee_link)      { "Paying your issue fee" }
-    let(:reducing_your_fee_link)    { "Help with fees" }
     let(:writing_yours_link)        { "Writing your claim statement" }
 
     let(:time_limits_div)           { "#time_limits" }
     let(:acas_div)                  { "#acas_early_conciliation" }
     let(:acas_exceptions_div)       { "#acas_early_conciliation_exceptions" }
-    let(:fees_div)                  { "#fees_and_payment" }
-    let(:working_out_your_fee_div)  { "#fees_and_payment_working_out_your_fee" }
-    let(:paying_your_fee_div)       { "#fees_and_payment_paying_your_fee" }
-    let(:reducing_your_fee_div)     { "#reducing_fees" }
     let(:writing_yours_div)         { "#writing_your_claim_statement" }
 
     scenario "User can click time time_limits_link" do
@@ -61,29 +45,11 @@ feature 'Guide', type: :feature do
       expect(page.find(acas_exceptions_div)).not_to be_nil
     end
 
-    scenario "User can click time fees_link" do
-      expect(page).to have_link(fees_link, href: "#{guide_path + fees_div}")
-      expect(page.find(fees_div)).not_to be_nil
-    end
-
-    scenario "User can click time working_out_your_fee_link" do
-      expect(page).to have_link(working_out_your_fee_link, href: "#{guide_path + working_out_your_fee_div}")
-      expect(page.find(working_out_your_fee_div)).not_to be_nil
-    end
-
-    scenario "User can click time paying_your_fee_link" do
-      expect(page).to have_link(paying_your_fee_link, href: "#{guide_path + paying_your_fee_div}")
-      expect(page.find(paying_your_fee_div)).not_to be_nil
-    end
-
-    scenario "User can click time reducing_your_fee_link" do
-      expect(page).to have_link(reducing_your_fee_link, href: "#{guide_path + reducing_your_fee_div}")
-      expect(page.find(reducing_your_fee_div)).not_to be_nil
-    end
-
-    scenario "User can click time writing_yours_link" do
-      expect(page).to have_link(writing_yours_link, href: "#{guide_path + writing_yours_div}")
-      expect(page.find(writing_yours_div)).not_to be_nil
+    scenario "User cannot see any fee related guides" do
+      expect(page).not_to have_text('Fees and payment')
+      expect(page).not_to have_text('Working out your fee')
+      expect(page).not_to have_text('Paying your issue fee')
+      expect(page).not_to have_text('Help with fees')
     end
   end
 
