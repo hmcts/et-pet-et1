27e5c1639fff30702ceb3c06f06812bbddb8898c
Gary Taylor <gary.taylor@hismessages.com>
2017-08-21 10:11:30 +0000

    Debug code to show html and current url on failure

--- features/support/debug.rb
+++ features/support/debug.rb
@@ -1,4 +1,8 @@
 After do |scenario|
   if scenario.failed?
+    puts "Page url was :-"
+    puts Capybara.current_session.current_url
+    puts "HTML Was :-"
+    puts Capybara.current_session.body
   end
 end
