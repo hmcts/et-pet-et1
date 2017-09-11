3b3c63e4ac33ddb990f68b8d0ee8e831fa0fb06b
Gary Taylor <gary.taylor@hismessages.com>
2017-09-11 08:57:45 +0000

    Removed previous half second delay on all features - did not work.  Added delay before clicking button in step one

--- features/step_definitions/step_one_steps.rb
+++ features/step_definitions/step_one_steps.rb
@@ -1,5 +1,9 @@
 And(/^I save my claim with a valid email address and password$/) do
   step_one_page.email.set('test@digital.justice.gov.uk')
   step_one_page.memorable_word.set('password')
+  # The sleep below should not be required - but for some reason the button does not seem to be clickable
+  # when run in the CI environment - it says Element is not clickable at point (123, 921)
+  sleep 0.5
+  # @TODO Remove once real reason is discovered and fixed
   step_one_page.save_and_continue.click
 end
