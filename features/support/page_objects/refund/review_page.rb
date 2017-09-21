48ccd4a1673a121c42438b781afc89b31bf92fdb
Gary Taylor <gary.taylor@hismessages.com>
2017-09-21 12:39:08 +0000

    Declaration text added

--- features/support/page_objects/refund/review_page.rb
+++ features/support/page_objects/refund/review_page.rb
@@ -1,8 +1,12 @@
 module Refunds
   class ReviewPage < BasePage
     section :declaration, :xpath, (XPath.generate {|x| x.descendant(:fieldset)[x.descendant(:legend)[x.string.n.is("Declaration")]]}) do
+      element :accept, 'I confirm the above'
       def set(value)
-        choose(value)
+        within @root_element do
+          check 'refunds_review[accept_declaration]' if value == 'Yes'
+          uncheck 'refunds_review[accept_declaration]' if value == 'No'
+        end
       end
     end
     element :save_and_continue, 'form.edit_refunds_review input[value="Submit Claim"]'
