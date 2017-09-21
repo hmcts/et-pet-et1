cee84ebb113a8fe5176dac9a2eba644674adb75a
Gary Taylor <gary.taylor@hismessages.com>
2017-09-21 12:01:42 +0000

    Minor text change

--- features/support/page_objects/refund/step_five_page.rb
+++ features/support/page_objects/refund/step_five_page.rb
@@ -9,11 +9,11 @@ module Refunds
       section :sort_code, AppTest::FormInput, :simple_form_field, 'Bank sort code'
     end
     section :building_society_details, :xpath, (XPath.generate {|x| x.descendant(:fieldset)[x.descendant(:legend)[x.string.n.is("Your Building Society Details")]]}) do
-      section :account_name, AppTest::FormInput, :simple_form_field, 'Building society account holder name'
-      section :building_society_name, AppTest::FormInput, :simple_form_field, 'Building society name'
-      section :account_number, AppTest::FormInput, :simple_form_field, 'Building society account number'
-      section :sort_code, AppTest::FormInput, :simple_form_field, 'Building society sort code'
-      section :reference_number, AppTest::FormInput, :simple_form_field, 'Building society roll/reference number'
+      section :account_name, AppTest::FormInput, :simple_form_field, 'Building Society account holder name'
+      section :building_society_name, AppTest::FormInput, :simple_form_field, 'Building Society name'
+      section :account_number, AppTest::FormInput, :simple_form_field, 'Building Society account number'
+      section :sort_code, AppTest::FormInput, :simple_form_field, 'Building Society sort code'
+      section :reference_number, AppTest::FormInput, :simple_form_field, 'Building Society roll/reference number'
     end
     element :save_and_continue, 'form.edit_refunds_bank_details input[value="Continue"]'
   end
