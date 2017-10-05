12c53945bdbbc5b0dd665feb80115a9d9e5ffee1
Gary Taylor <gary.taylor@hismessages.com>
2017-10-05 12:16:49 +0000

    Added unit test for confirmation form

---
+++ spec/forms/refunds/confirmation_form_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+module Refunds
+  RSpec.describe ConfirmationForm, type: :form do
+    let(:refund) { instance_spy(Refund) }
+    let(:form) { described_class.new(refund) }
+
+    it_behaves_like 'a Form', {  }, Refund
+  end
+end
