4ec1364b4639bc1636aee34e28dfb327e3b13e10
Gary Taylor <gary.taylor@hismessages.com>
2017-10-09 10:40:21 +0000

    Changed sessions table to use json instead of jsonb

--- db/migrate/20171006062918_create_sessions.rb
+++ db/migrate/20171006062918_create_sessions.rb
@@ -1,7 +1,7 @@
 class CreateSessions < ActiveRecord::Migration
   def change
     create_table :sessions do |t|
-      t.jsonb :data, default: {}
+      t.json :data, default: {}
 
       t.timestamps null: false
     end
