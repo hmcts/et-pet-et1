fee583ad7bf9919286711f809c0ccda408d093e4
Gary Taylor <gary.taylor@hismessages.com>
2017-10-11 08:28:34 +0000

    Initial state problem fixed on applicant page

--- app/assets/javascripts/pages/refunds/applicant.js
+++ app/assets/javascripts/pages/refunds/applicant.js
@@ -1,16 +1,30 @@
-var applicant = (function() {
+var refundApplicantPage = (function() {
+    var refundApplicantPage = {};
+
+    function selectedOptionEl() {
+        return $('.main-content.refunds_applicant [name=\'refunds_applicant[has_name_changed]\']:checked');
+    }
+
     function publishCurrentValues() {
-        var val = $('.main-content.refunds_applicant [name=\'refunds_applicant[has_name_changed]\']:checked').val();
+        var val = selectedOptionEl().val();
         $.publish('has_name_changed', val);
     }
     function enableContinueWhenAllowed() {
-        $.subscribe('has_name_changed', function (event, val) {
-            $('.main-content.refunds_applicant [data-behavior=\'continue_button\']').prop('disabled', val !== 'false');
-        });
+        $.subscribe('has_name_changed', setCurrentState);
     }
-    enableContinueWhenAllowed();
-    publishCurrentValues();
-});
-(function() {
-    $(document).ready(applicant);
+
+    function continueButtonEl() {
+        return $('.main-content.refunds_applicant [data-behavior=\'continue_button\']');
+    }
+
+    function setCurrentState() {
+        val = selectedOptionEl().val()
+        continueButtonEl().prop('disabled', val !== 'false');
+    }
+    refundApplicantPage.init = function () {
+        setCurrentState();
+        enableContinueWhenAllowed();
+        publishCurrentValues();
+    };
+    return refundApplicantPage
 })();
