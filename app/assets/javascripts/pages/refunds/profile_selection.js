2c63c071963fcc6f1554ac1d6f38893de6cc7b31
Gary Taylor <gary.taylor@hismessages.com>
2017-10-11 08:19:39 +0000

    Initial state problem fixed on profile selection page

--- app/assets/javascripts/pages/refunds/profile_selection.js
+++
@@ -1,9 +0,0 @@
-var profileSelection = (function() {
-    $('.main-content.refunds_profile_selection [data-behavior=\'continue_button\']').prop('disabled', true);
-    $('.main-content.refunds_profile_selection [data-behavior=\'profile_type\']').on('click', function() {
-        $('[data-behavior=\'continue_button\']').prop('disabled', false)
-    });
-});
-(function() {
-    $(document).ready(profileSelection);
-})();
