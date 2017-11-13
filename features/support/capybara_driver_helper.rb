94df518bf735f3eea4d7a1140b307ef5a1f57be9
Gary Taylor <gary.taylor@hismessages.com>
2017-11-13 11:18:22 +0000

    Experiment to use 4 parallel processes on travis

--- features/support/capybara_driver_helper.rb
+++ features/support/capybara_driver_helper.rb
@@ -46,5 +46,4 @@ Capybara.current_driver = Capybara.default_driver
 Capybara.always_include_port = true
 Capybara.app_host = ENV.fetch('CAPYBARA_APP_HOST', "http://#{ENV.fetch('HOSTNAME')}")
 Capybara.server_host = ENV.fetch('CAPYBARA_SERVER_HOST', ENV.fetch('HOSTNAME'))
-#Capybara.server_port = ENV.fetch('CAPYBARA_SERVER_PORT', '3000')
 Capybara.server = :webrick, { Logger: WEBrick::Log.new(Rails.logger, WEBrick::Log::DEBUG) }
