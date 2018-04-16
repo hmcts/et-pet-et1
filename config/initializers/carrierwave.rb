991f4de78940a028cbd9884c144bb6d3abcc4837
garytaylor <gary.taylor@hismessages.com>
2018-04-16 00:44:53 -0700

    Improvement to carrierwave config to allow the AWS endpoint and path_style options to be configurable from the environment.  This is for use with a fake s3 service. (#913)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,14 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   credentials = {
     provider:              'AWS',
-    region:                'eu-west-1',
+    region:                ENV.fetch('AWS_REGION', 'eu-west-1'),
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'] || '',
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || ''
   }
+
+  credentials.merge!(endpoint: ENV['AWS_ENDPOINT'], host: URI.parse(ENV['AWS_ENDPOINT']).host) if ENV.key?('AWS_ENDPOINT')
+
+  credentials[:path_style] = true if ENV.fetch('AWS_S3_FORCE_PATH_STYLE', 'false') == 'true'
 
   config.fog_public      = false
   config.fog_directory   = ENV['S3_UPLOAD_BUCKET']
