c09781e56c6a5a367697c9c2ea13cc0444f2d01f
Petr Zaparka <petr@zaparka.cz>
2018-06-15 12:49:38 +0100

    Saving results into db and allowing editing the responses.

--- app/assets/javascripts/pages/diversity/diverstity_ethnicity_page.js
+++ app/assets/javascripts/pages/diversity/diverstity_ethnicity_page.js
@@ -1,28 +1,38 @@
-var diversityEthinicityPage = (function() {
+var diversityEthinicityPage = (function(){
     var diversityEthinicityPage = {};
 
-    function subscribeToEthnicitySwitch() {
+    function subscribeToEthnicitySwitch(){
       $('select#diversity_ethnicity').change('change', function(e){
         hide_subgroups();
         var selected = $(this).val();
-        $(`*[data-name='${selected}']`).show()
+        $(`*[data-name='${selected}']`).show();
       })
     }
 
+    function show_selected_subgroup(){
+      var selected = $('select#diversity_ethnicity').val();
+      $(`*[data-name='${selected}']`).show();
+    }
+
     function hide_subgroups(){
-      $('fieldset.ethnicity_subgroup').hide()
-      resetSelections()
+      $('fieldset.ethnicity_subgroup').hide();
+      resetSelections();
     }
 
     function resetSelections(){
+      var subgroup_value = $('#diversity_ethnicity_subgroup').val();
       $('fieldset.ethnicity_subgroup select').each(function(i, element) {
-        $(element).val('')
+        if($(element).val() !== subgroup_value){
+          $(element).val('');
+        }
       });
+      $('#diversity_ethnicity_subgroup').val('')
     }
 
     diversityEthinicityPage.init = function() {
       hide_subgroups();
       subscribeToEthnicitySwitch();
+      show_selected_subgroup();
     };
     return diversityEthinicityPage;
 })();
--- spec/features/diversity_page_spec.rb
+++ spec/features/diversity_page_spec.rb
@@ -16,6 +16,69 @@ feature 'Diversity' do
     expect(page).to have_text "Diversity monitoring questionnaire"
     click_link "Begin this form"
 
+    fill_the_questionaire
+
+    click_button "Next step"
+
+    claim_type = find(:xpath,".//table//tr[1]").text
+    sex = find(:xpath,".//table//tr[2]").text
+    identity = find(:xpath,".//table//tr[3]").text
+    age = find(:xpath,".//table//tr[4]").text
+    ethnicity = find(:xpath,".//table//tr[5]").text
+    ethnicity_sugroup = find(:xpath,".//table//tr[6]").text
+    disability = find(:xpath,".//table//tr[7]").text
+    caring = find(:xpath,".//table//tr[8]").text
+    gender = find(:xpath,".//table//tr[9]").text
+    gender_birth = find(:xpath,".//table//tr[10]").text
+    pregnancy = find(:xpath,".//table//tr[11]").text
+    relationship = find(:xpath,".//table//tr[12]").text
+    religion = find(:xpath,".//table//tr[13]").text
+
+    expect(claim_type).to eql "Please confirm your type of claim to help us analyse other information provided in this form.Discrimination"
+    expect(sex).to eql "What is your sex?Male"
+    expect(identity).to eql "Which of the options below best describes your sexual identity?Other"
+    expect(age).to eql "Which age group are you in?25-34"
+    expect(ethnicity).to eql "What is your ethnic group?White"
+    expect(ethnicity_sugroup).to eql "What type is your ethnic group?Irish"
+    expect(disability).to eql "Do you have any physical or mental health conditions or illnesses lasting or expected to last for 12 months or more?No"
+    expect(caring).to eql "Do you have any caring responsibilities (eg, children, elderly relatives, partners etc)?No"
+    expect(gender).to eql "Please select your gender identity.Male (including female-to-male trans men)"
+    expect(gender_birth).to eql "Is your gender identity different to the sex you were assumed to be at birth?Yes"
+    expect(pregnancy).to eql "Were you pregnant when you were dismissed?No"
+    expect(relationship).to eql "Please describe your relationship status.Married"
+    expect(religion).to eql "What is your religion?Hindu"
+
+    click_link "Submit questionnaire"
+    expect(page).to have_text "Thank you for submitting a diversity questionnaire."
+  end
+
+
+  scenario "I can edit my responses" do
+    expect(page).to have_text "Diversity monitoring questionnaire"
+    click_link "Begin this form"
+
+    fill_the_questionaire
+    click_button "Next step"
+
+    expect(page).to have_text "Diversity monitoring questionnaire summary"
+
+    sex = find(:xpath,".//table/tr[2]").text
+    expect(sex).to eql('What is your sex?Male')
+
+    click_link "Edit your answers"
+
+    expect(page).to have_text "Editing Diversity monitoring questionnaire"
+
+    select "Female", from: 'Sex (optional)'
+    click_button "Next step"
+
+    expect(page).to have_text "Diversity monitoring questionnaire summary"
+    sex = find(:xpath,".//table/tr[2]").text
+    expect(sex).to eql('What is your sex?Female')
+  end
+
+
+  def fill_the_questionaire
     select "Discrimination", from: 'Claim type'
     select "Male", from: 'Sex (optional)'
     select "Other", from: 'Sexual identity'
@@ -33,7 +96,5 @@ feature 'Diversity' do
     select "No", from: 'Caring responsibility'
     select "No", from: 'Pregnancy'
     select "No", from: 'diversity_disability'
-
-    click_button "Submit questionnaire"
   end
 end
