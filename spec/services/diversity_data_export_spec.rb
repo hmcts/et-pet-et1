4cfbbc7781fc4eec5fa0147b47574f47f0c0d216
Petr Zaparka <petr@zaparka.cz>
2018-07-09 11:44:14 +0100

    Sending diversity data via DiversityDataExport lib

--- spec/services/diversity_data_export_spec.rb
+++ spec/services/diversity_data_export_spec.rb
@@ -1,48 +1,51 @@
 require "rails_helper"
 
 RSpec.describe DiversityDataExport, type: :service do
-  let(:diversity) {  build_stubbed(:diversity) }
+  ENV['ET_API_URL'] = 'http://127.0.0.1:3000/api/v2'
+
+  let(:diversity) { build_stubbed(:diversity) }
   let(:uuid) { "246b26b3-e383-4b68-b5b7-6d4ed6c76093" }
-  let(:diversity_data) {{
-    body:
-      { command: "BuildDiversityResponse",
-       data:
-        {claim_type: "Discrimination",
-         sex: "Prefer not to say",
-         sexual_identity: "Bisexual",
-         age_group: "35-44",
-         ethnicity: "Asian / Asian British",
-         ethnicity_subgroup: "Chinese",
-         disability: "No",
-         caring_responsibility: "No",
-         gender: "Female (including male-to-female trans women)",
-         gender_at_birth: "No",
-         pregnancy: "No",
-         relationship: "Married",
-         religion: "Jewish" },
-        uuid: uuid }
-      }
+  let(:uri) { URI.join('http://127.0.0.1:3000/api/v2', '/diversity/build_diversity_response') }
+  let(:httpx) { class_double(HTTPX) }
+  let(:diversity_data) {
+    {
+      command: "BuildDiversityResponse",
+      data:
+      { claim_type: "Discrimination",
+        sex: "Prefer not to say",
+        sexual_identity: "Bisexual",
+        age_group: "35-44",
+        ethnicity: "Asian / Asian British",
+        ethnicity_subgroup: "Chinese",
+        disability: "No",
+        caring_responsibility: "No",
+        gender: "Female (including male-to-female trans women)",
+        gender_at_birth: "No",
+        pregnancy: "No",
+        relationship: "Married",
+        religion: "Jewish" },
+      uuid: uuid
+    }
   }
+  let(:headers) { { 'Accept' => 'application/json' } }
 
   describe 'send the diversity data' do
     before do
       allow(Diversity).to receive(:find).and_return diversity
-      allow(SecureRandom).to receive(:uuid).and_return uuid
+      allow(HTTPX).to receive(:timeout).with(total_timeout: 10).and_return httpx
     end
 
-    it 'as post via HTTParty' do
-      dde = DiversityDataExport.new(diversity.id)
-      expect(HTTParty).to receive(:post).with("/diversity/build_diversity_response", diversity_data)
+    it 'as post via HTTPX' do
+      dde = DiversityDataExport.new(diversity.id, uuid)
+      expect(httpx).to receive(:post).with(uri, headers: headers, json: diversity_data)
       dde.send_data
     end
 
-    context 'failed call' do
-      it 'as post via HTTParty' do
-        dde = DiversityDataExport.new(diversity.id)
-        HTTParty.stub(:post).and_raise("Cannot connect")
-        HTTParty.should_receive(:post).at_least(3)
-        dde.send_data
-      end
+    it 'I can read response of the call' do
+      dde = DiversityDataExport.new(diversity.id, uuid)
+      allow(httpx).to receive(:post).and_return 'response'
+      dde.send_data
+      expect(dde.response).to eql('response')
     end
   end
 end
