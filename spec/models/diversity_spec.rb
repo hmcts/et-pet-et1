fe23c3c20bfa30cbcea27c93c239eb0a88bddc67
Petr Zaparka <petr@zaparka.cz>
2018-07-10 12:08:15 +0100

    Sending data to API after diversity is created.
    Refactoring diversity export class and add more tests.

--- spec/models/diversity_spec.rb
+++ spec/models/diversity_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Diversity, type: :model do
-  let(:diversity) { Diversity.new(religion: 'describe') }
+  let(:diversity) { build(:diversity, religion: 'describe') }
+  let(:uuid) { '246b26b3-e383-4b68-b5b7-6d4ed6c76093' }
 
   describe '#fill_religion' do
     it 'save the free text value if present' do
@@ -14,6 +15,24 @@ RSpec.describe Diversity, type: :model do
       diversity.religion_text = ''
       diversity.save
       expect(diversity.reload.religion).to eql('describe')
+    end
+  end
+
+  describe 'send_the_data to ET API' do
+    before do
+      allow(SecureRandom).to receive(:uuid).and_return uuid
+    end
+
+    it "after create" do
+      expect(DiversityFormJob).to receive(:perform_later).with(kind_of(Numeric), uuid)
+      diversity.save
+    end
+
+    it "not after save" do
+      diversity.save
+      expect(DiversityFormJob).not_to receive(:perform_later).with(diversity.id, uuid)
+      diversity.religion = 'test'
+      diversity.save
     end
   end
 end
