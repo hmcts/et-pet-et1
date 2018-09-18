423dc6f872e399109a54b4edfeaa76f083a41862
Petr Zaparka <petr@zaparka.cz>
2018-09-18 15:35:11 +0100

    Terms and conditions are using html template
    There is no need to use .md template and it makes the multilingual version more difficult. This is the new T&C page so the copy is not exactly same ans the previous T&Cs.

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,7 +1,2 @@
-class TermsController < MarkdownController
-  add_markdown_path views_term_markdowdon_link
-  add_markdown_files [
-    'general', 'applicable_law', 'applicable_law_responsible_use',
-    'data_protection', 'privacy_policy', 'disclaimer'
-  ]
+class TermsController < ApplicationController
 end
