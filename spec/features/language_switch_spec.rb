67d40be3303c0ca0127cbbcefad8207ec638af5f
Petr Zaparka <petr@zaparka.cz>
2018-09-26 10:58:48 +0100

    Bugfix persistance of language in forms
    When you choose the welsh and submit the form then it went directly to english again. I'm not sure if it's beacuse of a Rails 5 issue or I missed something but I fixed it by adding the locale to session.

--- spec/features/language_switch_spec.rb
+++ spec/features/language_switch_spec.rb
@@ -15,4 +15,17 @@ RSpec.feature 'Switching language', type: :feature do
     expect(page).to have_text("Make a claim to an employment tribunal")
     expect(page).to have_link("Cymraeg", href: '/apply?locale=cy')
   end
+
+  scenario 'langauge should persist after submitting the form' do
+    visit root_path
+    click_button 'Start a claim'
+
+    expect(page).to have_text('Saving your claim')
+    click_link('Cymraeg')
+
+    expect(page).to have_text('Cadw eich hawliad')
+    click_button('Cadw a pharhau')
+    expect(page).to have_text('Mae gwallau ar y dudalen')
+    expect(page).to have_text("Dylech greu gair cofiadwy er mwyn i chi allu dychwelyd i'ch hawliad")
+  end
 end
