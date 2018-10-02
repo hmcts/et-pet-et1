f880f41f4ef857f8365ec679f33851ca1d16e277
Gary Taylor <gary.taylor@hismessages.com>
2018-10-02 14:04:16 +0100

    Stubbing create reference in feature tests

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -4,10 +4,12 @@ selenium_url = URI.parse ENV.fetch('SELENIUM_URL', 'http://localhost:4444/wd/hub
 app_host_url = URI.parse Capybara.app_host
 et_api_url = 'http://api.et.net:4000/api/v2'
 build_claim_url = "#{et_api_url}/claims/build_claim"
+create_reference_url =  "#{et_api_url}/references/create_reference"
 WebMock.disable_net_connect!(allow_localhost: true, allow: [selenium_url.host, app_host_url.host])
 Around('@mock_et_api') do |_scenario, block|
   ClimateControl.modify ET_API_URL: et_api_url do
     stub_request(:post, build_claim_url).with(headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }).to_return(body: '{}', status: 202, headers: { 'Content-Type': 'application/json' })
+    stub_request(:post, create_reference_url).with(headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }).to_return(body: { status: 'created', data: { reference: 'somereference' } }.to_json, status: 201, headers: { 'Content-Type': 'application/json' })
     block.call
   end
 end
