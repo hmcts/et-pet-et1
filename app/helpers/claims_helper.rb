eaea6da08a1f74e60d6fadd7d54de9126ce6f7be
Petr Zaparka <petr@zaparka.cz>
2018-10-15 14:37:26 +0100

    Displaying feedback link based on language
    The second feedback link points outside of the app and there is no Welsh version for it. So Cathie ask for the link to be same as the one in header when the language version is Welsh.

--- app/helpers/claims_helper.rb
+++ app/helpers/claims_helper.rb
@@ -17,6 +17,13 @@ module ClaimsHelper
     [[t('.true'), true], [t('.false'), false]]
   end
 
+  def language_specific_feedback_url
+    if I18n.locale.to_s == 'en'
+      return 'https://www.gov.uk/done/employment-tribunals-make-a-claim'
+    end
+    "/apply/feedback?locale=#{I18n.locale}"
+  end
+
   private def markdown
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   end
