e89af8266be91a0a4679559d2f6c08fdcfd87a02
Petr Zaparka <petr@zaparka.cz>
2018-10-16 11:31:28 +0100

    Welsh address for office
    If the office is Wales and the translation is Welsh the address is loaded from translation file. For the rest the address is loaded from DB which is in English.

--- spec/presenters/confirmation_presenter_spec.rb
+++ spec/presenters/confirmation_presenter_spec.rb
@@ -18,7 +18,29 @@ RSpec.describe ConfirmationPresenter, type: :presenter do
           expect(confirmation_presenter.submission_information).
             to eq 'Submitted 01 January 2014 to tribunal office Brum, Brum lane, B1 1AA'
         end
+
+        context 'Welsh locale' do
+          before { I18n.locale = :cy }
+
+          it 'has the submitted date, the name, and address of the fee centre' do
+            expect(confirmation_presenter.submission_information).
+              to eq 'Cyflwynwyd ar 01 Ionawr 2014 i swyddfa tribiwnlys Brum, Brum lane, B1 1AA'
+          end
+
+          context 'Welsh address' do
+            before do
+              claim.build_office name: 'Wales', address: 'Employment Tribunal, Cardiff, CF24 0RZ'
+            end
+
+            it 'has the submitted date, the name, and address of the fee centre' do
+              expect(confirmation_presenter.submission_information).
+                to eq 'Cyflwynwyd ar 01 Ionawr 2014 i swyddfa tribiwnlys Cymru, Tribiwnlys Cyflogaeth, 3ydd Llawr, Llys Ynadon Caerdydd a’r Fro, Plas Fitzalan, Caerdydd, CF24 0RA'
+            end
+          end
+        end
+
       end
+
 
       context 'and claimants are seeking remission' do
         before { claim.remission_claimant_count = 1 }
