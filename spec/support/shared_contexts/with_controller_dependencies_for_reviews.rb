05b085310ade64b32db9c8b4c207205496563955
Gary Taylor <gary.taylor@hismessages.com>
2018-10-17 20:56:25 +0100

    Refactored common setup from view specs into shared context

---
+++ spec/support/shared_contexts/with_controller_dependencies_for_reviews.rb
@@ -0,0 +1,16 @@
+shared_context 'with controller dependencies for reviews' do
+  before do
+    controller.singleton_class.class_eval %Q{
+        include Rails.application.routes.url_helpers
+        protected
+        def claim
+          Claim.find(#{claim.id})
+        end
+        def claim_path_for(page, options = {})
+          send "claim_\#{page}_path".underscore, options
+        end
+
+        helper_method :claim, :claim_path_for
+      }
+  end
+end
--- spec/views/claim_reviews/show/review_page_claim_outcome_spec.rb
+++ spec/views/claim_reviews/show/review_page_claim_outcome_spec.rb
@@ -2,58 +2,42 @@ require "rails_helper"
 
 describe "claim_reviews/show.html.slim" do
   context "claim_outcome" do
-
-    before do
-      controller.singleton_class.class_eval %Q{
-        include Rails.application.routes.url_helpers
-        protected
-        def claim
-          Claim.find(#{claim.id})
-        end
-        def claim_path_for(page, options = {})
-          send "claim_\#{page}_path".underscore, options
-        end
-
-        helper_method :claim, :claim_path_for
-      }
-    end
+    include_context 'with controller dependencies for reviews'
     let(:review_page) do
       ET1::Test::ReviewPage.new
     end
 
-    context 'temp' do
-      let(:null_object) { NullObject.new }
-      before do
-        render template: "claim_reviews/show", locals: {
-          primary_claimant: claim.primary_claimant || null_object,
-          representative: claim.representative || null_object,
-          employment: claim.employment || null_object,
-          respondent: claim.primary_respondent || null_object,
-          secondary_claimants: claim.secondary_claimants,
-          secondary_respondents: claim.secondary_respondents
-        }
-        review_page.load(rendered)
-      end
+    let(:null_object) { NullObject.new }
+    before do
+      render template: "claim_reviews/show", locals: {
+        primary_claimant: claim.primary_claimant || null_object,
+        representative: claim.representative || null_object,
+        employment: claim.employment || null_object,
+        respondent: claim.primary_respondent || null_object,
+        secondary_claimants: claim.secondary_claimants,
+        secondary_respondents: claim.secondary_respondents
+      }
+      review_page.load(rendered)
+    end
 
-      let(:claim) do
-        create :claim, desired_outcomes: [:tribunal_recommendation, :new_employment_and_compensation],
-                        other_outcome: "25 bags\r\nyour job"
-      end
+    let(:claim) do
+      create :claim, desired_outcomes: [:tribunal_recommendation, :new_employment_and_compensation],
+        other_outcome: "25 bags\r\nyour job"
+    end
 
-      context 'Outcome details' do
-        subject { review_page.claim_outcome_section.what_outcome.answer.native.inner_html }
-        it do
-          is_expected.
-            to eq "A recommendation from a tribunal (that the employer takes action so that the problem at work doesn’t happen again)" \
+    context 'Outcome details' do
+      subject { review_page.claim_outcome_section.what_outcome.answer.native.inner_html }
+      it do
+        is_expected.
+          to eq "A recommendation from a tribunal (that the employer takes action so that the problem at work doesn’t happen again)" \
             "<br>To get another job with the same employer or associated employer"
-        end
       end
+    end
 
 
-      context 'What outcome' do
-        subject { review_page.claim_outcome_section.outcome_details.answer.native.inner_html }
-        it { is_expected.to eq("25 bags\n<br>your job") }
-      end
+    context 'What outcome' do
+      subject { review_page.claim_outcome_section.outcome_details.answer.native.inner_html }
+      it { is_expected.to eq("25 bags\n<br>your job") }
     end
   end
 end
