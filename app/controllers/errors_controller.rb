e2f07c8abc8a8b82de5b65191799ea269d18bc85
Petr Zaparka <petr@zaparka.cz>
2018-11-08 13:46:50 +0000

    Adding the missing html format respons

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,14 +3,14 @@ class ErrorsController < ApplicationController
   def not_found
     respond_to do |format|
       format.json { render json: { status: 404, error: 'Not Found' } }
-      format.json { render(status: 404) }
+      format.html { render(status: 404) }
     end
   end
 
   def unprocessable
     respond_to do |format|
       format.json { render json: { status: 422, error: 'Unprocessable Entity' } }
-      format.json { render(status: 422) }
+      format.html { render(status: 422) }
     end
 
   end
@@ -18,7 +18,7 @@ class ErrorsController < ApplicationController
   def internal_server_error
     respond_to do |format|
       format.json { render json: { status: 500, error: 'Internal Server Error' } }
-      format.json { render(status: 500) }
+      format.html { render(status: 500) }
     end
 
   end
@@ -26,7 +26,7 @@ class ErrorsController < ApplicationController
   def service_unavailable
     respond_to do |format|
       format.json { render json: { status: 503, error: 'Service Unavailable' } }
-      format.json { render(status: 503) }
+      format.html { render(status: 503) }
     end
 
   end
