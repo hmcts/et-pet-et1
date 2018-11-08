d202b8b32128229c3d1b476667c18e11563b24a7
Petr Zaparka <petr@zaparka.cz>
2018-11-08 11:15:36 +0000

    Adding json format responses for the errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,18 +1,33 @@
 class ErrorsController < ApplicationController
 
   def not_found
-    render(status: 404)
+    respond_to do |format|
+      format.json { render json: { status: 404, error: 'Not Found' } }
+      format.json { render(status: 404) }
+    end
   end
 
   def unprocessable
-    render(status: 422)
+    respond_to do |format|
+      format.json { render json: { status: 422, error: 'Unprocessable Entity' } }
+      format.json { render(status: 422) }
+    end
+
   end
 
   def internal_server_error
-    render(status: 500)
+    respond_to do |format|
+      format.json { render json: { status: 500, error: 'Internal Server Error' } }
+      format.json { render(status: 500) }
+    end
+
   end
 
   def service_unavailable
-    render(status: 503)
+    respond_to do |format|
+      format.json { render json: { status: 503, error: 'Service Unavailable' } }
+      format.json { render(status: 503) }
+    end
+
   end
 end
