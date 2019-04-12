01b71bb89496a39a02c9cbccd06a66a542551914
garytaylor <gary.taylor@hismessages.com>
2019-04-12 11:44:19 +0100

    The log level is now configured by RAILS_LOG_LEVEL env var (#1011)
    Added sidekiq_alive gem ready for azure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'debug').to_sym
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,3 +9,5 @@ Sidekiq.configure_client do |config|
   redis_config[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD'].present?
   config.redis = redis_config
 end
+
+Sidekiq::Logging.logger.level = ::Logger.const_get(ENV.fetch('RAILS_LOG_LEVEL', 'debug').upcase)
