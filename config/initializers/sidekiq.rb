1d50451f761a2a851d32da290dd20f2f386fecb2
Gary Taylor <gary.taylor@hismessages.com>
2019-04-17 17:15:19 +0100

    Revert "run.sh can now be used to run locally (full system)" pushed by accident
    This reverts commit 2c07fa178f2296c4d87c7d12885a8a474724a5b6.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,19 +1,11 @@
-default_redis_host = ENV.fetch('REDIS_HOST', 'localhost')
-default_redis_port = ENV.fetch('REDIS_PORT', '6379')
-default_redis_database = ENV.fetch('REDIS_DATABASE', '1')
-default_redis_url = "redis://#{default_redis_host}:#{default_redis_port}/#{default_redis_database}"
-redis_url = ENV.fetch('REDIS_URL', default_redis_url)
-
 Sidekiq.configure_server do |config|
-  redis_config = { url: redis_url }
+  redis_config = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
   redis_config[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD'].present?
   config.redis = redis_config
 end
 
-Sidekiq::Logging.logger.level = ::Logger.const_get(ENV.fetch('RAILS_LOG_LEVEL', 'debug').upcase)
-
 Sidekiq.configure_client do |config|
-  redis_config = { url: redis_url }
+  redis_config = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
   redis_config[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD'].present?
   config.redis = redis_config
 end
--- run.sh
+++ run.sh
@@ -13,18 +13,11 @@ create)
     ;;
 esac
 
-# remove if statement after migration to Azure is complete.
-# python ./awslogs-agent-setup.py -n -r eu-west-1 -c ./awslogs.conf
-if [[ $CLOUD_PROVIDER == "azure" || $DISABLE_CLOUDWATCH == "true" ]]; then
-# ps -eaf | grep awslogs | grep -v grep | awk -F' ' '{print $2'} | xargs kill -9
-    echo "Running app without cloudwatch"
-# supervisord -c /etc/supervisor.conf &
-else
-  ./expand_variables.sh
-  python ./awslogs-agent-setup.py -n -r eu-west-1 -c ./awslogs.conf
-  ps -eaf | grep awslogs | grep -v grep | awk -F' ' '{print $2'} | xargs kill -9
-  supervisord -c /etc/supervisor.conf &
-  echo "Running app with cloudwatch"
-fi
+./expand_variables.sh
+python ./awslogs-agent-setup.py -n -r eu-west-1 -c ./awslogs.conf
+ps -eaf | grep awslogs | grep -v grep | awk -F' ' '{print $2'} | xargs kill -9
+supervisord -c /etc/supervisor.conf &
 
-bundle exec unicorn -p ${PORT:-8080} -c ./config/unicorn.rb -E ${RAILS_ENV:-production}
+echo "Running app"
+
+bundle exec unicorn -p 8080 -c ./config/unicorn.rb -E production
