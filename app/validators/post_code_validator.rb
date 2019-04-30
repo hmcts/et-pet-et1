8a2fae64fe7950ace1d595f72f58435cf7757acf
Gary Taylor <gary.taylor@hismessages.com>
2019-04-30 10:36:43 +0100

    Update uk_postcode

--- app/validators/post_code_validator.rb
+++ app/validators/post_code_validator.rb
@@ -1,7 +1,7 @@
 class PostCodeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.present?
-      postcode = UKPostcode.new(value)
+      postcode = UKPostcode.parse(value)
 
       record.errors.add(attribute, :invalid) unless postcode.valid? && postcode.full?
     end
