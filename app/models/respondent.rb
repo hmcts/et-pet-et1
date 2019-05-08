d135974e22e52c371e140754b62e3ddde641a49d
Gary Taylor <gary.taylor@hismessages.com>
2019-05-08 06:10:21 +0100

    Remove fee group reference job call

--- app/models/respondent.rb
+++ app/models/respondent.rb
@@ -10,7 +10,6 @@ class Respondent < ApplicationRecord
   delegate(*ADDRESS_ATTRIBUTES, to: :address, prefix: true)
   delegate(*ADDRESS_ATTRIBUTES, to: :work_address, prefix: true)
 
-  before_save :enqueue_fee_group_reference_request, if: :needs_fee_group_reference?
 
   def address
     addresses.detect(&:primary?) || addresses.build(primary: true)
@@ -18,21 +17,5 @@ class Respondent < ApplicationRecord
 
   def work_address
     addresses.reject(&:primary?).first || addresses.build(primary: false)
-  end
-
-  private
-
-  def fee_group_reference_address
-    [work_address, address].find { |a| a.post_code.present? }
-  end
-
-  def enqueue_fee_group_reference_request
-    FeeGroupReferenceJob.perform_later claim, fee_group_reference_address.post_code
-  end
-
-  delegate :post_code_changed?, to: :fee_group_reference_address, allow_nil: true, prefix: true
-
-  def needs_fee_group_reference?
-    primary_respondent? && fee_group_reference_address_post_code_changed?
   end
 end
