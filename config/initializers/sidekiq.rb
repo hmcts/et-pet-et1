a94e84986b9bb0e809a2012ab289cf71ebd603e7
garytaylor <gary.taylor@hismessages.com>
2019-10-15 09:15:44 +0100

    RST-2264 (API, CCD, ET1, Admin) - Upgrade to sidekiq 6 (#1126)
    * RST-2264 (API, CCD, ET1, Admin) - Upgrade to sidekiq 6
    
    * RST-2264 (API, CCD, ET1, Admin) - Upgrade to sidekiq 6
    
    * RST-2264 (API, CCD, ET1, Admin) - Upgrade to sidekiq 6

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,12 +10,10 @@ Sidekiq.configure_server do |config|
   config.redis = redis_config
 end
 
-Sidekiq::Logging.logger.level = ::Logger.const_get(ENV.fetch('RAILS_LOG_LEVEL', 'debug').upcase)
-
 Sidekiq.configure_client do |config|
   redis_config = { url: redis_url }
   redis_config[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD'].present?
   config.redis = redis_config
 end
 
-Sidekiq::Logging.logger.level = ::Logger.const_get(ENV.fetch('RAILS_LOG_LEVEL', 'debug').upcase)
+Sidekiq.logger.level = ::Logger.const_get(ENV.fetch('RAILS_LOG_LEVEL', 'debug').upcase)
