60ab5f58bc7b801a18cbdf2c5e35b4a92f33586b
Gary Taylor <gary.taylor@hismessages.com>
2020-05-21 09:14:38 +0100

    RST-2591 - The moj template is now done - well, first iteration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import {EtGdsDesignSystem } from "et_gds_design_system"
+import "et_gds_design_system/stylesheet"
+EtGdsDesignSystem.initAll();
