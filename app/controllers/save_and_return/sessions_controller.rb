d875b08d4b0db8c621323833940dae19e4b59cea
Gary Taylor <gary.taylor@hismessages.com>
2020-06-04 10:31:02 +0100

    RST-2668 Forgot Memorable Word - Registration and login now working

--- app/controllers/save_and_return/sessions_controller.rb
+++ app/controllers/save_and_return/sessions_controller.rb
@@ -3,14 +3,14 @@ module SaveAndReturn
 
     def create
       super
-      return unless current_claim.present?
-
-      session[:claim_reference] = current_claim.application_reference
+      session[:claim_reference] = current_user.reference
     end
 
-    def claim_root_path
+    def user_root_path
       # @TODO Make this go to whatever page it needs to
-      claim_claimant_path
+      claim_path_for ClaimPagesManager.second_page
     end
+
+    protected
   end
 end
