0529943770b07f988978784f2c9bfea7c52d788a
garytaylor <gary.taylor@hismessages.com>
2020-07-22 07:38:09 +0100

    Rst 2672 (#1256)
    * Rst 2591 preparation (#1228)
    
    * RST-2591 - Ripped out old moj template code
    
    * RST-2591 - Old moj template added in to layouts temporarily and removed use of 'config_item' method
    
    * RST-2591 - Added and configured webpacker
    
    * RST-2591 - Upgraded et_gds_design_system to allow for install task
    
    * RST-2591 - Downgraded sprockets back to old version for now - might still be needed by admin
    
    * RST-2591 - Updated et_gds_design_system
    
    * RST-2591 - The moj template is now done - well, first iteration
    
    * RST-2591 - Removed old assets
    
    * RST-2591 - Removed old assets and switched to using webpack
    Also used the 'application_number' page as the first test page - so it is 70 %done
    
    * RST-2607 Fixed remainder of claimants details page (#1232)
    
    * RST-2672 Additional claimants page - Majority of test failures are now because the additional respondents page isnt done (another ticket)
    
    * RST-2672 Additional claimants page - Fixed additional claimants form
    
    * RST-2672 Additional claimants page - Group claims upload fixes
    
    * RST-2672 Additional claimants page
    
    * RST-2672 Additional claimants page
    
    * RST-2672 Added cuprite browser (chrome) - VERY fast

---
+++ app/forms/concerns/validate_nested.rb
@@ -0,0 +1,38 @@
+module ValidateNested
+  extend ActiveSupport::Concern
+
+  private
+
+  # Validate the associated records
+  def validate_collection_association(collection_method)
+    records = send(collection_method)
+    records.each_with_index { |record, index| collection_valid?(collection_method, record, index) }
+  end
+
+  # Returns whether or not the collection is valid and applies any errors to
+  # the parent, <tt>self</tt>, if it wasn't.
+  def collection_valid?(collection_method, record, index = nil)
+    unless valid = record.valid?
+      record.errors.each do |attribute, message|
+        attribute = normalize_collection_attribute(collection_method, index, attribute)
+        errors[attribute] << message
+        errors[attribute].uniq!
+      end
+
+      record.errors.details.each_key do |attribute|
+        collection_attribute =
+          normalize_collection_attribute(collection_method, index, attribute).to_sym
+
+        record.errors.details[attribute].each do |error|
+          errors.details[collection_attribute] << error
+          errors.details[collection_attribute].uniq!
+        end
+      end
+    end
+    valid
+  end
+
+  def normalize_collection_attribute(collection_method, index, attribute)
+    "#{collection_method}[#{index}].#{attribute}"
+  end
+end
