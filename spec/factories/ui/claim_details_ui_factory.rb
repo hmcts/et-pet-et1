63cf5b964d7c426d5242f6282c0ad832bdc6ca6e
garytaylor <gary.taylor@hismessages.com>
2020-12-07 18:24:12 +0000

    Rst 3025 (#1310)
    * RST-3025 GDS Component Upgrade - Claim details page
    
    * RST-3025 Claim details page now using new form builder

---
+++ spec/factories/ui/claim_details_ui_factory.rb
@@ -0,0 +1,19 @@
+module ET1
+  module Test
+    ClaimDetailsUi = Struct.new('ClaimDetailsUi', :text, :rtf_file_path, :other_known_claimants, :other_known_claimant_names, keyword_init: true)
+  end
+end
+FactoryBot.define do
+  factory :ui_claim_details, class: ::ET1::Test::ClaimDetailsUi do
+    trait :mandatory do
+      text { 'Claim details text' }
+      rtf_file_path { nil }
+      other_known_claimants { :'claim_details.other_known_claimants.options.no' }
+      other_known_claimant_names { nil }
+    end
+
+    trait :default do
+      mandatory
+    end
+  end
+end
