a72013ae357ea7c64afc86e56676433589f32a72
garytaylor <gary.taylor@hismessages.com>
2020-12-09 10:56:34 +0000

    Rst 2799 (#1312)
    * RST-2799 Locale changes for discrimination
    
    * RST-2799 Claim type page now using gds
    
    * RST-2799 Fixes to locale files from merge conflict

--- spec/support/page_objects/about_the_claim_page.rb
+++ spec/support/page_objects/about_the_claim_page.rb
@@ -7,9 +7,42 @@ module ET1
 
       # Clicks the save and continue button
       def save_and_continue
-
+        save_and_continue_button.submit
       end
 
+      def assert_questions
+        expect(page).not_to have_text("Unfair dismissal (including constructive dismissal) (optional)", wait: false)
+        expect(page).to have_text("Unfair dismissal (including constructive dismissal)")
+
+        expect(page).not_to have_text("Protective Award (optional)", wait: false)
+        expect(page).to have_text("Protective Award")
+
+        expect(page).not_to have_text("Other type of claim (optional)", wait: false)
+        expect(page).to have_text("Other type of claim")
+
+        expect(page).not_to have_text("Are you reporting suspected wrongdoing at work? (optional)", wait: false)
+        expect(page).to have_text("Are you reporting suspected wrongdoing at work?")
+        self
+      end
+
+      section :whistle_blowing_fieldset, :govuk_fieldset, :'claim_type.is_whistleblowing.heading' do
+        include EtTestHelpers::Section
+
+        # @!method whistle_blowing_question
+        #   A govuk radio button component for whistle blowing question
+        #   @return [EtTestHelpers::Components::GovUKCollectionRadioButtons] The site prism section
+        section :whistle_blowing_question, govuk_component(:collection_radio_buttons), :govuk_collection_radio_buttons, :'claim_type.is_whistleblowing.label'
+
+        # @!method whistle_blowing_body_question
+        #   A govuk radio button component for whistle blowing send to body question
+        #   @return [EtTestHelpers::Components::GovUKCollectionRadioButtons] The site prism section
+        section :whistle_blowing_body_question, govuk_component(:collection_radio_buttons), :govuk_collection_radio_buttons, :'claim_type.send_claim_to_whistleblowing_entity.label'
+      end
+
+      # @!method save_and_continue_button
+      #   A govuk submit button component...
+      #   @return [EtTestHelpers::Components::GovUKSubmit] The site prism section
+      section :save_and_continue_button, govuk_component(:submit), :govuk_submit, :'claim_type.save_and_continue'
     end
   end
 end
