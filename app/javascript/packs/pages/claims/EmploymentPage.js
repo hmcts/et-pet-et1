42475bf903660efbc6d9f467e905c965441f6b6f
Gary Taylor <gary.taylor@hismessages.com>
2021-02-01 07:43:42 +0000

    RST-2804 Employment page styling sorted

--- app/javascript/packs/pages/claims/EmploymentPage.js
+++ app/javascript/packs/pages/claims/EmploymentPage.js
@@ -3,7 +3,7 @@ import RevealOnRadioButton from '../../components/RevealOnRadioButton'
 function setupReveal() {
   document.querySelectorAll('*[data-reveal-on-selector]').forEach(function (node) {
     const selector = node.attributes['data-reveal-on-selector'].value;
-    const value = node.attributes['data-reveal-on-value'].value;
+    const value = JSON.parse(node.attributes['data-reveal-on-value'].value);
     RevealOnRadioButton(node, selector, value);
   });
 }
