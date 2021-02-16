e18103692ef974004f4a1ab409b37136d77ec71f
garytaylor <gary.taylor@hismessages.com>
2021-02-16 15:36:24 +0000

    Rst 2804 - Resolve styling issues (#1351)
    * RST-2804 Respondent page now looks same as original
    RST-2804 Representative page now looks same as original
    RST-2804 Group claims page now looking same as original
    RST-2804 Claimants details page looking identical to original
    RST-2804 Home page now looking identical to original
    RST-2804 Starting to compare pages side by side and fixing issues
    
    * RST-2804 Additional respondents page now looks same as original
    
    * RST-2804 First attempt at a form builder method for reveal sections
    
    * RST-2804 Employment page styling sorted
    
    * RST-2804 Claim type styline sorted
    
    * RST-2804 Re visited all pages using new form builder methods to make code simpler and easier to read.
    RST-2804 Re visited all pages applying correct styling (except save and return - this is in another ticket)
    
    * RST-2804 Added bin/sidekiq
    
    * RST-2804 rebased on top of latest 2590

---
+++ app/javascript/packs/pages/claims/ClaimConfirmationPage.js
@@ -0,0 +1,26 @@
+function monitorDownloadLink() {
+  const successNode = document.querySelector('.pdf-success');
+  const failureNode = document.querySelector('.pdf-failure');
+  const url = successNode.getAttribute('href')
+  let http = new XMLHttpRequest();
+  http.overrideMimeType("application/pdf");
+  http.open('GET', url, true);
+  http.onload = function (e) {
+    if (http.readyState === 4) {
+      if (http.status === 200) {
+        failureNode.style.display = 'none';
+        successNode.style.display = 'block';
+      } else {
+        failureNode.style.display = 'block';
+        successNode.style.display = 'none';
+        console.warn("Unable to find PDF, retrying " + url + " in 10 seconds");
+        setTimeout(function() { monitorDownloadLink() }, 10000);
+      }
+    }
+  };
+  http.send();
+}
+
+export default function ClaimConfirmationPage() {
+  monitorDownloadLink();
+}
