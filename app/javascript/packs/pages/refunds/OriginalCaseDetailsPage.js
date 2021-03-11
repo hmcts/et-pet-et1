707266ded13e11b57892bc672d1f382a9c01143c
Gary Taylor <gary.taylor@hismessages.com>
2021-03-11 14:20:29 +0000

    RST-2802 Original case details done

--- app/javascript/packs/pages/refunds/OriginalCaseDetailsPage.js
+++ app/javascript/packs/pages/refunds/OriginalCaseDetailsPage.js
@@ -1,11 +1,9 @@
 function OriginalCaseDetailsPage() {
   function publishCurrentAddressChanged() {
-    let val = $('.main-content.refunds_original_case_details [name=\'refunds_original_case_details[address_changed]\']:checked').val();
-    $.publish('address_changed', val);
+    let val = $(".refunds_original_case_details [name='refunds_original_case_details[address_changed]']:checked").val();
   }
   function publishCurrentHadRepresentative() {
-    let val = $('.main-content.refunds_original_case_details [name=\'refunds_original_case_details[claim_had_representative]\']:checked').val();
-    $.publish('claim_had_representative', val);
+    let val = $(".refunds_original_case_details [name='refunds_original_case_details[address_changed]']:checked").val();
   }
   function publishCurrentValues() {
     publishCurrentAddressChanged();
