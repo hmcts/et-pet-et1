4da93a82f5ab8cb3f53b32a6feff17f28cd3921d
garytaylor <gary.taylor@hismessages.com>
2021-03-17 06:26:20 +0000

    Rst 3118 (#1373)
    * RST-2804 Respondent page now looks same as original
    RST-2804 Representative page now looks same as original
    RST-2804 Group claims page now looking same as original
    RST-2804 Claimants details page looking identical to original
    RST-2804 Home page now looking identical to original
    RST-2804 Starting to compare pages side by side and fixing issues
    
    * RST-2804 Re visited all pages using new form builder methods to make code simpler and easier to read.
    RST-2804 Re visited all pages applying correct styling (except save and return - this is in another ticket)
    
    * RST-3118 Switched admin over to using webpack and removed sprockets and sass gem

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "ET Fees Claim Metadata"
-
+  config.use_webpacker = true
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
