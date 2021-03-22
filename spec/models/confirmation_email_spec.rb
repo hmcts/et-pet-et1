212595fdad0edcf15523cd4cc87b7ac5fce7ab19
Gary Taylor <gary.taylor@hismessages.com>
2021-03-22 12:15:14 +0000

    RST-3225 Re designed the code around email recipients at the review stage.  Old code relied on some functionality within simple_form

--- spec/models/confirmation_email_spec.rb
+++ spec/models/confirmation_email_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 describe ConfirmationEmail do
-  let(:confirmation_email) { described_class.new }
+  let(:claim) { create(:claim) }
+  let(:confirmation_email) { described_class.new(claim) }
 
-  it 'has 5 slots for email addresses' do
-    expect(confirmation_email.additional_email_address.size).to eq(5)
+  describe '#initialize' do
+    it 'has the correct values for the email_addresses' do
+      expect(confirmation_email.email_addresses).to contain_exactly(claim.primary_claimant.email_address)
+    end
+  end
+
+  describe '#all_email_addresses' do
+    it 'has the correct values' do
+      expect(confirmation_email.all_email_addresses).to contain_exactly([claim.primary_claimant.email_address])
+    end
   end
 end
