c18f267ec379d67d60212d587c940d388b8b8b1f
frederic jonquieres <fjonquieres83@gmail.com>
2021-04-22 14:00:28 +0100

    RST-3271 & Rst-3272 Fixed small issues for azure test.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,6 @@ require 'database_cleaner'
 # end with _spec.rb. You can configure this pattern with with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
-
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
