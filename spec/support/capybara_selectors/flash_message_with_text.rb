01e2b49ed02f2513407a9d5b44daaf041af86d6e
garytaylor <gary.taylor@hismessages.com>
2021-06-16 10:59:53 +0100

    RST-3226 Fixed test issue with session reloaded message in refunds (#1415)

--- spec/support/capybara_selectors/flash_message_with_text.rb
+++ spec/support/capybara_selectors/flash_message_with_text.rb
@@ -1,5 +1,5 @@
 Capybara.add_selector(:flash_message_with_text) do
   xpath do |locator, _options|
-    XPath.generate { |x| x.descendant(:h2)[x.ancestor(:div)[x.attr(:class).contains('flash').and(x.string.equals(locator))]] }
+    XPath.generate { |x| x.descendant(:p)[x.ancestor(:div)[x.attr(:class).contains_word('flash').and(x.string.equals(locator))]] }
   end
 end
