fd6eef09c7cae6e6dfa0fffd64d5ff8e6b5aa329
frederic jonquieres <fjonquieres83@gmail.com>
2022-01-25 14:50:31 +0000

    RST-2040 Fixed country migration

--- db/migrate/20220114153119_remove_default_value_for_country.rb
+++ db/migrate/20220114153119_remove_default_value_for_country.rb
@@ -1,5 +1,5 @@
 class RemoveDefaultValueForCountry < ActiveRecord::Migration[6.1]
   def change
-    change_column_default :addresses, :country, from: 'United Kingdom', to: nil
+    change_column_default :addresses, :country, from: 'united_kingdom', to: nil
   end
 end
