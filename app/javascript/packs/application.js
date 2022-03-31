4a613d236769a628af28c02a9b1efc26fc43a837
garytaylor <gary.taylor@hismessages.com>
2022-03-31 13:25:57 +0100

    RST-4363 application.js now loads core-js for polyfills (#1479)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,8 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'core-js';
+
 import AdditionalClaimantsUploadPage from "./pages/claims/AdditionalClaimantsUploadPage";
 
 const images = require.context('./csv', true);
