72ed15666225a1580f5fb1ea08ed29d05e05ccaf
kalvinlyu <kalvin.lyu@solirius.com>
2022-08-01 14:58:00 +0100

    RST-4585
    Migration to set is_other_type_of_claim to true or false.

--- db/migrate/20220729095120_update_is_other_type_of_claim_in_table_claims.rb
+++ db/migrate/20220729095120_update_is_other_type_of_claim_in_table_claims.rb
@@ -1,7 +1,6 @@
 class UpdateIsOtherTypeOfClaimInTableClaims < ActiveRecord::Migration[7.0]
   def up
-    execute("UPDATE claims SET is_other_type_of_claim = true WHERE other_claim_details IS NOT NULL;")
-    execute("UPDATE claims SET is_other_type_of_claim = false WHERE is_other_type_of_claim IS NULL;")
+    execute("UPDATE claims SET is_other_type_of_claim = (other_claim_detail IS NOT NULL);")
 
   end
 
