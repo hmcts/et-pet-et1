72f0c168180b83f29ad00df1e13bdbdcdcb89d32
kalvinlyu <kalvin.lyu@solirius.com>
2022-08-01 09:53:05 +0100

    RST-4548
    Migration to update current claims values.

--- db/migrate/20220729095120_update_is_other_type_of_claim_in_table_claims.rb
+++ db/migrate/20220729095120_update_is_other_type_of_claim_in_table_claims.rb
@@ -1,6 +1,8 @@
 class UpdateIsOtherTypeOfClaimInTableClaims < ActiveRecord::Migration[7.0]
   def up
     execute("UPDATE claims SET is_other_type_of_claim = true WHERE other_claim_details IS NOT NULL;")
+    execute("UPDATE claims SET is_other_type_of_claim = false WHERE is_other_type_of_claim IS NULL;")
+
   end
 
   def down
