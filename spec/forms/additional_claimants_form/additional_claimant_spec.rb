c149a80d9c48344656c754bce7beed6d1b3caa50
garytaylor <gary.taylor@hismessages.com>
2022-10-11 09:40:40 +0100

    RST-4712 Fixed issue with validation of multiple respondents (#1507)

--- spec/forms/additional_claimants_form/additional_claimant_spec.rb
+++ spec/forms/additional_claimants_form/additional_claimant_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe AdditionalClaimantsForm::AdditionalClaimant, type: :form do
 
     it { expect(additional_claimant).to validate_length_of(:address_building).is_at_most(50) }
     it { expect(additional_claimant).to validate_length_of(:address_street).is_at_most(50) }
-    it { expect(additional_claimant).to validate_length_of(:address_locality).is_at_most(25) }
-    it { expect(additional_claimant).to validate_length_of(:address_county).is_at_most(25) }
+    it { expect(additional_claimant).to validate_length_of(:address_locality).is_at_most(50) }
+    it { expect(additional_claimant).to validate_length_of(:address_county).is_at_most(50) }
     it { expect(additional_claimant).to validate_length_of(:address_post_code).is_at_most(8) }
   end
 
