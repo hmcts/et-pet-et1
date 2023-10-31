efa8baf5a78f56adf85c99bd16e001e29a068d94
kalvinlyu <kalvin.lyu@solirius.com>
2023-10-31 11:41:59 +0000

    RST-5641 made regex more efficient

--- app/validators/special_character_validator.rb
+++ app/validators/special_character_validator.rb
@@ -3,15 +3,11 @@ class SpecialCharacterValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.nil? || value.blank?
 
-    regex = if allow_comma == true && allow_numbers == true
-              /^[a-zA-Z0-9 ,'-]+$/i
-            elsif allow_comma == true
-              /^[a-zA-Z ,'-]+$/i
-            elsif allow_numbers == true
-              /^[a-zA-Z0-9 '-]+$/i
-            else
-              /^[a-zA-Z '-]+$/i
-            end
+    allowed_values = "a-zA-Z -'"
+    allowed_values << "0-9" if allow_numbers == true
+    allowed_values << "," if allow_comma == true
+
+    regex = /^[#{allowed_values}]+$/i
 
     record.errors.add(attribute, :contains_special_characters) unless value.match(regex)
   end
