e77f427c330a83b7c9c93e951c887f58a1ca454f
Gary Taylor <gary.taylor@hismessages.com>
2024-05-29 14:13:01 +0100

    (RST-6655) migration failure fixed

--- db/migrate/20240529102522_populate_claimant_phone_or_video_questions.rb
+++ db/migrate/20240529102522_populate_claimant_phone_or_video_questions.rb
@@ -4,8 +4,12 @@ class PopulateClaimantPhoneOrVideoQuestions < ActiveRecord::Migration[7.1]
   end
 
   def up
-    Claimant.where(allow_video_attendance: false).update_all(allow_phone_or_video_attendance: '["neither"]')
-    Claimant.where(allow_video_attendance: true).update_all(allow_phone_or_video_attendance: '["video"]')
+    Claimant.where(allow_video_attendance: false).all.each do |claimants|
+      claimant.update(allow_phone_or_video_attendance: ['neither'])
+    end
+    Claimant.where(allow_video_attendance: true).all.each do |claimant|
+      claimant.update(allow_phone_or_video_attendance: ['video'])
+    end
   end
 
   def down
