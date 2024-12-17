1abece41bbce5c680072d5efb7197f026b644935
DanielleKushnir <daniellekushnir22@gmail.com>
2024-12-17 13:40:22 +0000

    RST-6880 Fixed typo

--- app/forms/refunds/confirmation_form.rb
+++ app/forms/refunds/confirmation_form.rb
@@ -3,10 +3,10 @@ module Refunds
     attribute :application_reference, :string
     attribute :submitted_at, :et_date
 
-    def initialize(resource, &form)
+    def initialize(resource, &block)
       raise 'Session has no persisted refund' unless resource.respond_to?('_refund_id')
 
-      super(Refund.find_by!(id: resource._refund_id), &form)
+      super(Refund.find_by!(id: resource._refund_id), &block)
     end
   end
 end
