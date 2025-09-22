16a08fcdf333aee809fa45cfb47ab19d113e6816
garytaylor <gary.taylor@hismessages.com>
2025-09-22 13:09:21 +0100

    (RST-7392) Setup smoke test

--- lib/tasks/test.rake
+++ lib/tasks/test.rake
@@ -1,4 +1,8 @@
 require 'English'
+
+# Override Rails default test task to use RSpec
+Rake::Task["test"].clear if Rake::Task.task_defined?("test")
+
 task test: :environment do
   unless system("rspec -t ~smoke --format progress --format RspecJunitFormatter --out tmp/test/rspec.xml")
     raise "Rspec testing failed #{$CHILD_STATUS}"
@@ -6,7 +10,9 @@ task test: :environment do
 end
 
 task 'test:smoke' => :environment do
-  puts "No smoke tests yet"
+  unless system("rspec -t smoke --format progress --format RspecJunitFormatter --out tmp/test/rspec.xml spec/features/smoke_spec.rb")
+    raise "Rspec smoke testing failed #{$CHILD_STATUS}"
+  end
 end
 
 task 'test:functional' => :environment do
---
+++ spec/smoke_test_helper.rb
@@ -0,0 +1,17 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
+require 'shoulda/matchers'
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../config/environment', __dir__)
+require 'rspec/rails'
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
