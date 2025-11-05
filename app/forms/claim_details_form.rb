9231e44e14573cb6eaf867e10ded2e751a66b703
Gary Taylor <gary.taylor@hismessages.com>
2025-11-05 12:10:29 +0000

    (rst-3382) Allow multiple file types for additional information file

--- app/forms/claim_details_form.rb
+++ app/forms/claim_details_form.rb
@@ -15,7 +15,22 @@ class ClaimDetailsForm < Form
   validates :other_known_claimant_names, length: { maximum: 350 }
   validates :other_known_claimants, inclusion: [true, false]
   validates :claim_details_rtf, content_type: {
-    in: ['text/rtf', 'application/rtf', 'application/msword'], message: I18n.t('errors.messages.rtf')
+    in: [
+      'application/pdf', 'application/msword', 'application/x-msword', 'application/doc', 'application/vnd.ms-word',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'text/plain', 'application/txt',
+      'application/msword', 'application/x-msword', 'application/msword-template', 'image/jpeg', 'image/pjpeg', 'image/bmp',
+      'image/x-windows-bmp', 'image/tiff', 'image/x-tiff', 'image/png', 'image/x-png', 'application/vnd.ms-excel',
+      'application/msexcel', 'application/x-msexcel', 'application/x-ms-excel', 'application/vnd.ms-excel',
+      'application/x-excel', 'application/vnd.ms-excel', 'application/x-excel',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.template', 'application/vnd.ms-powerpoint',
+      'application/mspowerpoint', 'application/x-mspowerpoint', 'application/vnd.ms-powerpoint',
+      'application/vnd.ms-powerpoint', 'application/vnd.ms-powerpoint', 'application/x-mspowerpoint',
+      'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+      'application/vnd.openxmlformats-officedocument.presentationml.template',
+      'application/vnd.openxmlformats-officedocument.presentationml.slideshow', 'application/rtf', 'text/rtf', 'text/csv',
+      'application/csv', 'application/vnd.ms-excel'
+    ], message: I18n.t('errors.messages.rtf')
   }
 
   def claim_form_details_rtf?
